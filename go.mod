97a20580f8d145050b7fd0a60168a028bc0723de
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2019-04-19 21:05:26 +0200

    ui: plot widget

--- go.mod
+++ go.mod
@@ -7,7 +7,8 @@ require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/ktye/iv v0.0.0-20190320125319-a11fa0fedc5b
 	github.com/ktye/plot v0.0.0-20190418145510-8e0ab7910c77
-	github.com/ktye/ui v0.0.0-20190419102356-8d12a357199a
+	github.com/ktye/ui v0.0.0-20190419182601-89856430f7e2
 	github.com/mattn/go-sixel v0.0.0-20190320171103-a8fac8fa7d81
 	golang.org/x/image v0.0.0-20190417020941-4e30a6eb7d9a
+	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6
 )
--- ⍳/ui.go
+++ ⍳/ui.go
@@ -7,10 +7,16 @@ package main
 //	go build -tags ui
 
 import (
+	"image"
+
 	"github.com/eaburns/T/rope"
 	"github.com/ktye/iv/cmd/lui/font"
+	"github.com/ktye/plot"
 	"github.com/ktye/ui"
+	"golang.org/x/mobile/event/key"
 )
+
+var win *ui.Window
 
 func main() {
 	var interp interp
@@ -21,23 +27,34 @@ func main() {
 	repl.Interp = &interp
 	interp.a = kinit()
 
-	w := ui.New(nil)
-	w.SetFont(font.APL385(), 20)
-	w.Top.W = repl
-	w.Render()
+	p := interp.a["plot"].(plot.Plot)
+	p.Style.Dark = false
+	interp.a["plot"] = p
+
+	win = ui.New(nil)
+	win.SetFont(font.APL385(), 20)
+	win.Top.W = repl
+	win.Render()
 
 	for {
 		select {
-		case e := <-w.Inputs:
-			w.Input(e)
+		case e := <-win.Inputs:
+			win.Input(e)
 
-		case err, ok := <-w.Error:
+		case err, ok := <-win.Error:
 			if !ok {
 				return
 			}
 			println("ui:", err.Error())
 		}
 	}
+}
+
+func top(w ui.Widget) { // set the top widget
+	win.Top.W = w
+	win.Top.Layout = ui.Dirty
+	win.Top.Draw = ui.Dirty
+	win.Render()
 }
 
 type interp struct {
@@ -51,12 +68,39 @@ func (i *interp) Eval(s string) {
 	if x != nil {
 		s, o := x.(string)
 		if !o {
-			s = fmt(x).(string)
+			if p, o := x.(plot.Plot); o {
+				i.plot(plot.Plots{p})
+				s = ""
+			} else if p, o := x.(plot.Plots); o {
+				i.plot(p)
+				s = ""
+			} else {
+				s = fmt(x).(string)
+			}
 		}
 		i.repl.Write([]byte(s))
 	}
 	i.repl.Write([]byte{'\n', ' '})
 	i.repl.Edit.MarkAddr("$")
+}
+
+type plotui struct { // plotui overwrites the ESC button to return to the repl
+	ui.Plot
+	save ui.Widget
+}
+
+func (p *plotui) Key(w *ui.Window, self *ui.Kid, k key.Event, m ui.Mouse, orig image.Point) (r ui.Result) {
+	if k.Code == key.CodeEscape {
+		r.Consumed = true
+		top(p.save)
+	}
+	return
+}
+
+func (i *interp) plot(p plot.Plots) {
+	w := plotui{save: win.Top.W}
+	w.SetPlots(p)
+	top(&w)
 }
 
 func (i *interp) Cancel() {}
