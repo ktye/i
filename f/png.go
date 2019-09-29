24bd1e0dabaffd8a4b5bddad6b76c07f032150fa
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2019-09-29 14:24:58 +0200

    more fonts

---
+++ f/png.go
@@ -0,0 +1,98 @@
+// +build ignore
+
+package main
+
+// create png files for font in bdf and plan9 formats (assuming monospace).
+// run with
+//  go run png.go fontname.bdf  # creates fontname.png
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"fmt"
+
+	"github.com/zachomedia/go-bdf"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/plan9font"
+	"golang.org/x/image/math/fixed"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		panic("args")
+	}
+	var face font.Face
+	var off [2]int // dot position in character rectangle
+	var w, h int
+	name := os.Args[1]
+	if strings.HasSuffix(name, ".bdf") {
+		name = strings.TrimSuffix(name, ".bdf")
+		b, err := ioutil.ReadFile(name + ".bdf")
+		p(err)
+		f, err := bdf.Parse(b)
+		face = f.NewFace()
+		p(err)
+		h = f.Size
+		_, advance, _ := face.GlyphBounds('M')
+		w = advance.Floor()
+		off = f.Encoding['M'].LowerPoint
+	} else if strings.HasSuffix(name, ".font") {
+		data, err := ioutil.ReadFile(name)
+		p(err)
+		name = strings.TrimSuffix(name, ".font")
+		face, err = plan9font.ParseFont(data, func(rel string) ([]byte, error) { return ioutil.ReadFile(rel) })
+		p(err)
+		_, advance, _ := face.GlyphBounds('M')
+		w = advance.Floor()
+		h = face.Metrics().Height.Floor()
+		off[1] = -face.Metrics().Descent.Floor()
+		fmt.Printf("metrics: %v\n", face.Metrics())
+	} else {
+		panic("format")
+	}
+	ww, hh := 41+42*w, 3+4*h
+	println(w, h, ww, hh)
+
+	filename := name + "_" + strconv.Itoa(w) + "x" + strconv.Itoa(h) + ".png"
+	file, err := os.Create(filename)
+	p(err)
+	defer file.Close()
+	m := image.NewRGBA(image.Rect(0, 0, ww, hh))
+	red := color.RGBA{255, 0, 0, 255}
+	draw.Draw(m, m.Bounds(), &image.Uniform{red}, image.ZP, draw.Src)
+
+	s := make([]byte, 42)
+	copy(s, "????????????????")
+	copy(s[42-len(filename):], filename)
+	chars := []string{string(s), ":+-*%&|<>=!~,^#_$?@.0123456789'/\\;`\"(){}[]", "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
+	y := 0
+	for row := range chars {
+		x := 0
+		for col := range chars[row] {
+			glyph(m, face, chars[row][col], off, x, y, w, h)
+			x += 1 + w
+		}
+		y += 1 + h
+	}
+
+	p(png.Encode(file, m))
+}
+
+func glyph(m draw.Image, f font.Face, r byte, off [2]int, x, y, w, h int) {
+	draw.Draw(m, image.Rect(x, y, x+w, y+h), &image.Uniform{color.White}, image.ZP, draw.Src)
+	dot := fixed.P(x+off[0], y+h+off[1])
+	dr, mask, mp, _, _ := f.Glyph(dot, rune(r))
+	draw.DrawMask(m, dr, &image.Uniform{color.Black}, image.ZP, mask, mp, draw.Src)
+}
+
+func p(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
--- f/readme.md
+++ f/readme.md
@@ -1,6 +1,6 @@
 ```
 input files: 10x20.png, 16x32.png
-glyphs are black on white, separated with 1 red pixel padding
+glyphs are read from black pixels, 1 px padding (red)
 for 10x20, width: 461 (41+42*10), height: 3+4*20 (83)
 
 16 0123456789ABCDE (small double numbers for non-printable)
@@ -9,13 +9,42 @@ for 10x20, width: 461 (41+42*10), height: 3+4*20 (83)
 26 ABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 k font files are generated with:
+ go run gen.go lucx11_6x11.png > luc11.k
+ go run gen.go lucx13_7x13.png > luc13.k
+ go run gen.go zeev8x16.png 8 16 > f1.k
  go run gen.go 10x20.png 10 20 > f2.k
  go run gen.go 16x32.png 16 32 > f3.k
 
 /unpack (linear black-pixel indexes)
 \l f2.k
-font:{&,/(8#2)\:'0+x}'font
+font:{&,/+(8#2)\:'0+x}'font
 ```
 
+# 6x11, 7x13
+[6x11](lucx11_6x11.png)
+[7x13](lucx11_7x13.png)
+```
+from plan9 (lstr.10, lstr.12) (lucidasans?)
+ go run png.go lucx11.font
+ go run png.go lucx13.font
+```
+
+# 8x16
+![8x16](zeev8x16.png)
+```
+from zeev, 1px line with
+ http://zevv.nl/play/code/zevv-peep/zevv-peep-iso8859-1-08x16.bdf > zeev.bdf
+ go run png.go zeev.bdf
+```
+
+# 10x20
 ![10x20](10x20.png)
+```
+manual work, 2px line width, modified misc-10x20, taller thicker characters
+```
+
+# 16x32
 ![16x32](16x32.png)
+```
+manual work, 3px line width, proportions inspired by PragmataPro
+```
