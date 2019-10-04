f7cd8b0cb354022316674c491e60ca3e963d5c69
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2019-10-04 13:38:44 +0200

    k_test: remove most of it

--- k_test.go
+++ k_test.go
@@ -7,9 +7,7 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"math"
 	"os"
-	"reflect"
 	"strconv"
 	"testing"
 )
@@ -22,18 +20,6 @@ type (
 	iv = []int
 	sv = []string
 )
-
-func TestIni(t *testing.T) {
-	t.Skip()
-	ini()
-	st := Stats()
-	if st.UsedBlocks() != 1 {
-		t.Fatal()
-	}
-	//mk(1, 9000)
-	//pfl()
-	//xxd()
-}
 
 func TestK(t *testing.T) {
 	// t.Skip()
@@ -632,380 +618,6 @@ func TestK(t *testing.T) {
 		}
 	}
 }
-
-func TestParse(t *testing.T) {
-	// t.Skip()
-	testCases := []struct {
-		x, r, t s
-	}{
-		{"", "", "`"},
-		{"`a", ",`a", "`N"},
-		{"`a`b", ",`a`b", "`."},
-		{"`a / b", ",`a", "`N"},
-		{".x", "(`.;,`x)", "`."},
-		{"{1+x}", "{1+x}", "`1"},
-		{"{x+y}", "{x+y}", "`2"},
-		{"{1+(2;`a;y)}", "{1+(2;`a;y)}", "`2"},
-		{"/alpha\n`a", "(`;;,`a)", "`."},
-		{"/alpha\n`a /beta\n/gamma", "(`;;,`a;)", "`."},
-		{"0x01", "0x01", "`c"},
-		{"0xF", "0x0f", "`c"},
-		{"0x", `""`, "`C"},
-		{"0x1234", "0x1234", "`C"},
-		{`"a"`, `"a"`, "`c"},
-		{`"a\t\n\r\"xyz"`, `"a\t\n\r\"xyz"`, "`C"},
-		{"10", "10", "`i"},
-		{"10 20", "10 20", "`I"},
-		{"-3", "-3", "`i"},
-		{".1", "0.1", "`f"},
-		{"2.", "2f", "`f"},
-		{"-1.23e-005", "-1.23e-5", "`f"},
-		{"1 2f", "1 2f", "`F"},
-		{"1.23 3", "1.23 3", "`F"},
-		{"1 2 3. ", "1 2 3f", "`F"},
-		{"2i-3", "3.605551a303.6901", "`z"},
-		{"-2.0e+012i-3.6", "2e12a180", "`z"},
-		{"1 2 3. 2i-3", "1 2 3 3.605551a303.6901", "`Z"},
-		{"+", "+", "`2"},
-		{"+:", "+:", "`1"},
-		{"0:", "0:", "`2"},
-		{"0:1", "(0::;1)", "`."},
-		{"x-:", "(-:;`x)", "`."},
-		{"'", "'", "`2"},
-		{"1+2;", "(`;(+;1;2);)", "`."},
-		{"(/)", ",/", "`."},
-		{"2/3", "((/;2);3)", "`."},
-		{"(/:)", ",/:", "`."},
-		{"in", "in", "`2"},
-		{"within", "within", "`2"},
-		{"bin", "bin", "`2"},
-		{"like", "like", "`2"},
-		{"1 2@0", "(1 2;0)", "`."},
-		{"+/", "(/;+)", "`."},
-		{"+/1 2 3", "((/;+);1 2 3)", "`."},
-		{"1+/3 4 5", "((/;+);1;3 4 5)", "`."},
-		{"*1 2 3", "(*:;1 2 3)", "`."},
-		{"1+(2;`a;3.5)", "(+;1;(;2;,`a;3.5))", "`."},
-		{"1;2\n3", "(`;1;2;3)", "`."},
-		{"1 2 3[0 2]", "(1 2 3;0 2)", "`."},
-		{"`a`b`c[2]", "(,`a`b`c;2)", "`."},
-		{"x[0][1]", "((`x;0);1)", "`."},
-		{"(1;(2;3);4)[1;1]", "((;1;(;2;3);4);1;1)", "`."},
-		{"(1;(`a;3);4)[1;0]", "((;1;(;,`a;3);4);1;0)", "`."},
-		{"x[1;2;3]", "(`x;1;2;3)", "`."},
-		{"(`a`b!1 2)", "(!;,`a`b;1 2)", "`."},
-		{"(`a`b!1 2)[`b]", "((!;,`a`b;1 2);,`b)", "`."},
-		{"2+", "(+;2)", "`."},
-		{"{x+y}[2;3]", "({x+y};2;3)", "`."},
-		{"g:{x+y};g[3;4]", "(`;(:;`g;{x+y});(`g;3;4))", "`."},
-		{"-':8 2 5", "((':;-);8 2 5)", "`."},
-		{`|+\!3`, `(|:;((\;+);(!:;3)))`, "`."},
-		{"{-x}+3", `(+;{-x};3)`, "`."},
-		{"$[0;1;0;2;3]", "($;0;1;0;2;3)", "`."},
-		{"(-).(1 2)", "(.;,-;1 2)", "`."},
-		{"x+1", "(+;`x;1)", "`."},
-		{"x-1", "(-;`x;1)", "`."},
-		{"x -1", "(`x;-1)", "`."},
-		{"3.5-1", "(-;3.5;1)", "`."},
-		{"3.5 -1", "3.5 -1", "`F"},
-		{"x[1]-2", "(-;(`x;1);2)", "`."},
-		{"-*3", "(-:;(*:;3))", "`."},
-		{"+-", "(.;+:;-)", "`."},
-		{"+-:", "(.;+:;-:)", "`."},
-		{"+-*", "(.;+:;(.;-:;*))", "`."},
-		{"3*+", "(.;(*;3);+)", "`."},
-		{"3+2+", "(.;(+;3);(+;2))", "`."},
-		{". {$[x>3;2;3]}5", "(.:;({$[x>3;2;3]};5))", "`."},
-	}
-	for i, occ := range []bool{true, false} {
-		for j, tc := range testCases {
-			for _, at := range []bool{false, true} {
-				ini()
-				x := mkb([]c(tc.x))
-				if occ {
-					inc(x)
-				}
-				y := prs(x)
-				exp := tc.r
-				if at {
-					fmt.Printf("@`p(%q) ~ %v\n", tc.x, tc.t)
-					y = kst(tip(y))
-					exp = tc.t
-				} else {
-					fmt.Printf("`p(%q) ~ %v\n", tc.x, tc.r)
-					y = kst(y)
-				}
-				r := string(G(y).([]c))
-				if !reflect.DeepEqual(r, exp) {
-					t.Fatalf("[%d/%d]: expected: %v got %v\n", j, i, exp, r)
-				}
-				dec(y)
-				if occ {
-					dec(x)
-				}
-				clear()
-				check(t)
-			}
-		}
-	}
-}
-func TestNumMonad(t *testing.T) {
-	// t.Skip()
-	ini()
-	xv := []interface{}{c(3), []c{3, 5}, -5, iv{3, -9}, 3.2, []f{-3.5, 2.9, 0}, 2 - 4i, []z{4 - 2i, 3 + 4i}}
-	testCases := []struct {
-		f func(k) k
-		s s
-		r []interface{}
-	}{
-		{neg, "-", l{c(253), []c{253, 251}, 5, iv{-3, 9}, -3.2, []f{3.5, -2.9, -0}, -2 + 4i, []z{-4 + 2i, -3 - 4i}}},
-		{fst, "*", l{c(3), c(3), -5, 3, 3.2, -3.5, 2 - 4i, 4 - 2i}},
-		{rev, "|", l{c(3), []c{5, 3}, -5, iv{-9, 3}, 3.2, []f{0, 2.9, -3.5}, 2 - 4i, []z{3 + 4i, 4 - 2i}}},
-		{not, "~", l{0, iv{0, 0}, 0, iv{0, 0}, 0, iv{0, 0, 1}, 0, iv{0, 0}}},
-		{enl, ",", l{[]c{3}, l{[]c{3, 5}}, iv{-5}, l{iv{3, -9}}, []f{3.2}, l{[]f{-3.5, 2.9, 0}}, []z{2 - 4i}, l{[]z{4 - 2i, 3 + 4i}}}},
-		{cnt, "#", l{1, 2, 1, 2, 1, 3, 1, 2}},
-		{str, "$", l{c(3), []c{3, 5}, []c("-5"), l{[]c("3"), []c("-9")}, []c("3.2"), l{[]c("-3.5"), []c("2.9"), []c("0")}, []c("4.472136a296.5651"), l{[]c("4.472136a333.4349"), []c("5a53.1301")}}},
-		{kst, "`k@", l{[]c("0x03"), []c("0x0305"), []c("-5"), []c("3 -9"), []c("3.2"), []c("-3.5 2.9 0"), []c("4.472136a296.5651"), []c("4.472136a333.4349 5a53.1301")}},
-		{tip, "@", l{"c", "C", "i", "I", "f", "F", "z", "Z"}},
-		{evl, ".", xv},
-	}
-	for _, occ := range []bool{true, false} {
-		for j, tc := range testCases {
-			for i := range xv {
-				// fmt.Println("TC", xv[i])
-				x := K(xv[i])
-				if x == 0 {
-					t.Fatalf("cannot import go type %T", xv[i])
-				}
-				if occ {
-					inc(x)
-				}
-				y := tc.f(x)
-				if occ {
-					dec(x)
-				}
-				r := G(y)
-				fmt.Printf("%s(%v) = %v\n", tc.s, xv[i], r)
-				if !reflect.DeepEqual(r, tc.r[i]) {
-					t.Fatalf("[%d/%d]: expected: %v got %v (@%d)\n", j, i, tc.r[i], r, y)
-				}
-				dec(y)
-				clear()
-				check(t)
-			}
-		}
-	}
-}
-func TestMonad(t *testing.T) {
-	// t.Skip()
-	ini()
-	testCases := []struct {
-		f    func(k) k
-		s    s
-		x, r interface{}
-	}{
-		// flp needs ini
-		//{flp, "+", l{iv{1, 2}, iv{3, 4}}, l{iv{1, 3}, iv{2, 4}}},
-		//{flp, "+", l{iv{1, 2, 3}, []f{3, 4, 5}}, l{l{1, 3.0}, l{2, 4.0}, l{3, 5.0}}},
-		//{flp, "+", l{l{1, 2.2, l{3, 4}}, l{"a", []c{'x'}, 5 + 2i}}, l{l{1, "a"}, l{2.2, []c{'x'}}, l{l{3, 4}, 5 + 2i}}},
-		//{flp, "+", l{l{1, 2.2}, l{3, 4.4}}, l{iv{1, 3}, []f{2.2, 4.4}}},
-		{til, "!", 3, iv{0, 1, 2}},
-		{til, "!", -1, l{iv{1}}},
-		{til, "!", -3, l{iv{1, 0, 0}, iv{0, 1, 0}, iv{0, 0, 1}}},
-		{til, "!", d{sv{"a", "b"}, iv{1, 2}}, sv{"a", "b"}},
-		// TODO !a(odometer)
-		{fst, "*", l{3, 4, 5}, 3},
-		{fst, "*", "alpha", "alpha"},
-		{fst, "*", l{"alpha"}, "alpha"},
-		{fst, "*", d{l{"x", "y"}, l{iv{5, 3}, 4}}, iv{5, 3}},
-		{fst, "*", d{sv{"x", "y"}, iv{7, 2}}, 7},
-		{inv, "%", 4, 0.25},
-		{inv, "%", []f{4.0, 8.0}, []f{0.25, 0.125}},
-		{str, "$", "", []c("")},
-		{str, "$", "a", []c("a")},
-		{str, "$", sv{"", "a", "bb", "a\t\r\nb"}, l{[]c(""), []c("a"), []c("bb"), []c("a\t\r\nb")}},
-		{str, "$", l{1, c(3), l{4, 5.0}}, l{[]c("1"), c(3), l{[]c("4"), []c("5")}}},
-		{str, "$", d{sv{"x", "y"}, iv{1, 2}}, d{sv{"x", "y"}, l{[]c("1"), []c("2")}}},
-		{kst, "`k", iv{1, 2, 3}, []c("1 2 3")},
-		{kst, "`k", l{1, 2, l{4, 5}}, []c("(1;2;(4;5))")},
-		{kst, "`k", d{l{5, 5.5}, iv{1, 2}}, []c("(5;5.5)!1 2")},
-		{rev, "|", l{}, l{}},
-		{rev, "|", l{iv{3}}, l{iv{3}}},
-		{rev, "|", l{1, 2}, l{2, 1}},
-		{rev, "|", l{1, l{3, 4}}, l{l{3, 4}, 1}},
-		{rev, "|", d{iv{1, 2}, iv{3, 4}}, d{iv{2, 1}, iv{4, 3}}},
-		{rev, "|", d{sv{"alpha", "beta"}, l{3, iv{3, 5}}}, d{sv{"beta", "alpha"}, l{iv{3, 5}, 3}}},
-		{wer, "&", iv{0, 0, 1, 1, 0, 1}, iv{2, 3, 5}},
-		{wer, "&", iv{}, iv{}},
-		{wer, "&", iv{2}, iv{0, 0}},
-		{wer, "&", iv{1, 2, 3}, iv{0, 1, 1, 2, 2, 2}},
-		{asc, "<", iv{1, 2, 3, 4}, iv{0, 1, 2, 3}},
-		{asc, "<", iv{1, 4, 3, 2}, iv{0, 3, 2, 1}},
-		{asc, "<", iv{4, 2, 3, 4}, iv{1, 2, 0, 3}},
-		{asc, "<", []f{4, 1, 2}, iv{1, 2, 0}},
-		{asc, "<", []c{6, 4, 2, 1}, iv{3, 2, 1, 0}},
-		{asc, "<", []z{4, 1, 2}, iv{1, 2, 0}},
-		{asc, "<", []z{0, 1 + 1i, 1, 2}, iv{0, 2, 1, 3}},
-		{asc, "<", sv{"b", "ab", "a", "aa"}, iv{2, 3, 1, 0}},
-		{dsc, ">", iv{1, 4, 3, 2}, iv{1, 2, 3, 0}},
-		//{grp, "=", []c{'c', 'b', 'a', 'c', 'a', 'b', 'c'}, d{[]c{'c', 'b', 'a'}, l{iv{0, 3, 6}, iv{1, 5}, iv{2, 4}}}},
-		{grp, "=", iv{1, 2, 3}, d{iv{1, 2, 3}, l{iv{0}, iv{1}, iv{2}}}},
-		{grp, "=", iv{3, 3, 1, 3, 2, 1}, d{iv{1, 2, 3}, l{iv{2, 5}, iv{4}, iv{0, 1, 3}}}},
-		{grp, "=", []f{5.5, 1, 3, 3, 2}, d{[]f{1, 2, 3, 5.5}, l{iv{1}, iv{4}, iv{2, 3}, iv{0}}}},
-		{grp, "=", []z{3, 3 + 1i, 3, 3 + 1i, 3 + 1i}, d{[]z{3, 3 + 1i}, l{iv{0, 2}, iv{1, 3, 4}}}},
-		{grp, "=", sv{"alpha", "beta", "alpha", "gamma", "alpha", "beta"}, d{sv{"alpha", "beta", "gamma"}, l{iv{0, 2, 4}, iv{1, 5}, iv{3}}}},
-		{enl, ",", "alpha", sv{"alpha"}},
-		{enl, ",", l{1, 2, l{3, 4.5}}, l{l{1, 2, l{3, 4.5}}}},
-		{enl, ",", d{iv{3, 4}, sv{"x", "y"}}, l{d{iv{3, 4}, sv{"x", "y"}}}},
-		{srt, "^", iv{3, 1, 2, 3, 5}, iv{1, 2, 3, 3, 5}},
-		{cnt, "#", "alpha", 1},
-		{cnt, "#", l{}, 0},
-		{cnt, "#", l{1, 2, l{3, 4}}, 3},
-		{cnt, "#", d{iv{3, 4}, sv{"x", "y"}}, 2},
-		{tip, "@", l{}, "."},
-		{tip, "@", d{iv{1, 2}, iv{3, 4}}, "a"},
-		{evl, ".", l{uint16(2), l{uint16(2), 3}}, 3},
-		{evl, ".", l{uint16(2), l{uint16(6), iv{3, 4}}}, iv{-4, -3}},
-		{evl, ".", l{uint16(2), iv{3, 4}}, iv{-3, -4}},
-		{unq, "?", []c{1, 2, 43, 2}, []c{1, 2, 43}},
-		{unq, "?", iv{1, 2, 3, 2}, iv{1, 2, 3}},
-		{unq, "?", []f{5, 0, 0, 0, 8, 0, 0, 0, 5, 0, 0, 5}, []f{5, 0, 8}},
-		{unq, "?", []z{0, 4i, 5i, 4i, 0, 3}, []z{0, 4i, 5i, 3}},
-		{unq, "?", l{1, 2, 3, 1}, l{1, 2, 3}},
-		{unq, "?", l{1i, l{2, sv{"a"}}, l{3, "b"}, l{2, sv{"a"}}, 1i}, l{1i, l{2, sv{"a"}}, l{3, "b"}}},
-	}
-	for _, occ := range []bool{true, false} {
-		for j, tc := range testCases {
-			//fmt.Println("TC", i, j, tc.s, tc.x, "occ", occ)
-			x := K(tc.x)
-			_ = Stats().UsedBlocks()
-			if x == 0 {
-				t.Fatalf("cannot import go type %T", tc.x)
-			}
-			if occ {
-				inc(x)
-			}
-			y := tc.f(x)
-			fpck("1")
-			if occ {
-				dec(x)
-			}
-			r := G(y)
-			fmt.Printf("%s[%v] = %v\n", tc.s, tc.x, r)
-			if !reflect.DeepEqual(r, tc.r) {
-				t.Fatalf("monad[%d]: expected: %v got %v (@%d)\n", j, tc.r, r, y)
-			}
-			dec(y)
-			clear()
-			check(t)
-		}
-	}
-}
-func TestDyad(t *testing.T) {
-	// t.Skip()
-	testCases := []struct {
-		f       func(k, k) k
-		s       s
-		x, y, r interface{}
-	}{
-		{atx, "@", iv{2, 1, 3, 5}, iv{2, 0, 1}, iv{3, 2, 1}},
-		{ept, "^", iv{1, 5, 3, 3, 2}, iv{6, 3}, iv{1, 5, 2}},
-		{drp, "_", 2, iv{1, 2, 3, 4}, iv{3, 4}},
-		{drp, "_", -2, iv{1, 2, 3, 4}, iv{1, 2}},
-		{drp, "_", 2, l{1, 2.0, "a"}, sv{"a"}},
-		{drp, "_", -2, l{1, 2.0, "a"}, iv{1}},
-		{fnd, "?", iv{1, 2}, iv{2}, iv{1}},
-		{fnd, "?", []c{5, 4, 3, 2}, c(2), 3},
-	}
-	for _, occ := range []bool{true, false} {
-		for j, tc := range testCases {
-			// fmt.Println("TC", i, j, tc.s, tc.x, "occ", occ)
-			x := K(tc.x)
-			y := K(tc.y)
-			_ = Stats().UsedBlocks()
-			if x == 0 || y == 0 {
-				t.Fatalf("cannot import go type %T", tc.x)
-			}
-			if occ {
-				inc(x)
-				inc(y)
-			}
-			z := tc.f(x, y)
-			fpck("1")
-			if occ {
-				dec(x)
-				dec(y)
-			}
-			r := G(z)
-			fmt.Printf("%s[%v, %v] = %v\n", tc.s, tc.x, tc.y, r)
-			if !reflect.DeepEqual(r, tc.r) {
-				t.Fatalf("dyad[%d]: expected: %v got %v\n", j, tc.r, r)
-			}
-			dec(z)
-			clear()
-			check(t)
-		}
-	}
-}
-func TestTo(t *testing.T) {
-	// t.Skip()
-	ini()
-	testCases := []struct {
-		x, r interface{}
-		t    k
-	}{
-		{c(1), 1, I},
-		{c(1), 1.0, F},
-		{[]c{1, 2, 3, 4}, iv{1, 2, 3, 4}, I},
-		{1, c(1), C},
-		{5, 5.0, F},
-		{1, 1 + 0i, Z},
-		{iv{1, 2, 3, 4}, []z{1, 2, 3, 4}, Z},
-		{2.3, c(2), C},
-		{2.3, 2, I},
-		{2.3, 2.3 + 0i, Z},
-		{-2.3 + 4.5i, -2.3, F},
-		{2.3 - 4.5i, c(2), C},
-		{complex(math.NaN(), 0), -2147483648, I},
-		{complex(0, math.NaN()), -2147483648, I},
-	}
-	for _, occ := range []bool{true, false} {
-		for _, tc := range testCases {
-			x := K(tc.x)
-			if occ {
-				inc(x)
-			}
-			y := to(x, tc.t)
-			if occ {
-				dec(x)
-			}
-			r := G(y)
-			fmt.Printf("to(%v,%d) = %v\n", tc.x, tc.t, tc.r)
-			if !reflect.DeepEqual(r, tc.r) {
-				t.Fatalf("expected: %v got %v %T %T\n", tc.r, r, tc.r, r)
-			}
-			dec(y)
-			clear()
-			check(t)
-		}
-	}
-}
-func TestStr(t *testing.T) {
-	// t.Skip()
-	ini()
-	for _, x := range []s{"a", "b", "aa", "bb", "alpha", "betagamm"} {
-		n := len(x)
-		if n > 8 {
-			n = 8
-		}
-		if r := G(K(x)); r != x[:n] {
-			t.Fatalf("expected %s got %s\n", x, r)
-		}
-	}
-	if u := sym(8 + K("abcdefgh")<<2); u != 0x6162636465666768 {
-		t.Fatalf("%x\n", u)
-	}
-}
 func TestRef(t *testing.T) { // generate readme.md
 	k, e := ioutil.ReadFile("k.go")
 	if e != nil {
@@ -1119,10 +731,13 @@ func pfl() {
 		println(i, strconv.FormatUint(uint64(m.k[i]), 16), strconv.FormatUint(uint64(m.k[i]<<2), 16))
 	}
 }
-func xdec(x k) {
-	if m.k[x]>>28 != 0 {
-		dec(x)
-	}
+func pr(x k, a ...interface{}) {
+	fmt.Printf(":%x ", x)
+	r := kst(inc(x))
+	_, n := typ(r)
+	s := s(m.c[8+r<<2 : 8+n+r<<2])
+	dec(r)
+	fmt.Println(a, s)
 }
 func xxd() { // memory dump
 	h := k(0)
@@ -1186,25 +801,13 @@ func fpck(s s) { // check free pointers
 		}
 	}
 }
-func pr(x k, a ...interface{}) {
-	fmt.Printf(":%x ", x)
-	r := kst(inc(x))
-	_, n := typ(r)
-	s := s(m.c[8+r<<2 : 8+n+r<<2])
-	dec(r)
-	fmt.Println(a, s)
-}
+
 
 type Bucket struct {
-	Type       uint32
-	Used, Free uint32 // num blocks
-	Net        uint32
+	Type, Used uint32
 }
 type MemStats map[uint32]Bucket
 
-func (b Bucket) Overhead() uint32 {
-	return b.Used*uint32(1<<b.Type) - b.Net
-}
 func (s MemStats) UsedBlocks() (t uint32) {
 	for _, b := range s {
 		t += b.Used
@@ -1226,7 +829,6 @@ func Stats() MemStats {
 			}
 			b := st[t]
 			b.Type = t
-			b.Free++
 			st[t] = b
 			o = 1 << (t - 2)
 		} else {
@@ -1242,7 +844,6 @@ func Stats() MemStats {
 			if n == atom {
 				n = 1
 			}
-			b.Net += n * lns[tp]
 			st[t] = b
 			o = 1 << (t - 2)
 		}
@@ -1251,8 +852,7 @@ func Stats() MemStats {
 	return st
 }
 
-// type conversions between go and k (used by k_test.go)
-
+// type conversions between go and k
 func K(x interface{}) k { // convert go value to k type, returns 0 on error
 	if x == nil {
 		return mk(N, atom)
