b1f5d664102d9e53b36d8c1afed1f271e2c37912
U-DDA01\csd80117 <ktye@users.noreply.github.com>
2019-11-27 12:12:29 +0100

    drw

--- _/k/w.go
+++ _/k/w.go
@@ -1,4 +1,4 @@
-// +build !cgo
+// +build !windows
 
 package main
 
--- _/k/w_windows.go
+++ _/k/w_windows.go
@@ -16,217 +16,171 @@ func drw(x, y k) (r k) { // x 9:y  x(width `i) y(pixel-data `I)
 		panic("size")
 	}
 	p := 8 + y<<2
-	bitmap(w, h, m.c[p:p+4*w*h]).draw()
+	draw(w, h, m.c[p:p+4*w*h])
 	return decr(x, y, inc(null))
 }
 
-type winHANDLE uintptr
-type winHDC winHANDLE
-type winHWND winHANDLE
-type winHGDIOBJ winHANDLE
-type winHGLOBAL winHANDLE
-type winHBITMAP winHGDIOBJ
-type winBITMAP struct {
-	BmType       int32
-	BmWidth      int32
-	BmHeight     int32
-	BmWidthBytes int32
-	BmPlanes     uint16
-	BmBitsPixel  uint16
-	BmBits       unsafe.Pointer
+type wBM struct { // bitmap
+	a, b, c, d i
+	e, f       uint16
+	h          unsafe.Pointer
 }
-type winDIBSECTION struct {
-	DsBm        winBITMAP
-	DsBmih      winBITMAPINFOHEADER
-	DsBitfields [3]uint32
-	DshSection  winHANDLE
-	DsOffset    uint32
+type wDS struct { // dibsection
+	b wBM
+	h wBI
+	f [3]k
+	s uintptr
+	o k
 }
-type winCIEXYZ struct{ CiexyzX, CiexyzY, CiexyzZ int32 }
-type winCIEXYZTRIPLE struct{ CiexyzRed, CiexyzGreen, CiexyzBlue winCIEXYZ }
-type winBITMAPINFOHEADER struct {
-	BiSize                           uint32
-	BiWidth, BiHeight                int32
-	BiPlanes, BiBitCount             uint16
-	BiCompression, BiSizeImage       uint32
-	BiXPelsPerMeter, BiYPelsPerMeter int32
-	BiClrUsed, BiClrImportant        uint32
+type wcxyz struct{ x, y, z i }   // ciexyz
+type wc3 struct{ r, g, b wcxyz } // ciexyztriple
+type wBI struct {                // bitmapinfoheader
+	s    k
+	w, h i
+	p, c uint16
+	m, i k
+	x, y i
+	u, t k
 }
-type winBITMAPV4HEADER struct {
-	winBITMAPINFOHEADER
-	BV4RedMask    uint32
-	BV4GreenMask  uint32
-	BV4BlueMask   uint32
-	BV4AlphaMask  uint32
-	BV4CSType     uint32
-	BV4Endpoints  winCIEXYZTRIPLE
-	BV4GammaRed   uint32
-	BV4GammaGreen uint32
-	BV4GammaBlue  uint32
+type wBM4 struct { // bitmapv4header
+	wBI
+	r, g, b, a, t k
+	e             wc3
+	rr, gg, bb    k
 }
-type winBITMAPV5HEADER struct {
-	winBITMAPV4HEADER
-	BV5Intent, BV5ProfileData, BV5ProfileSize, BV5Reserved uint32
+type wBM5 struct { // bitmapv5header
+	wBM4
+	a5, b5, c5, d5 k
+}
+type bitmap struct {
+	m, p uintptr
+	w, h i
+}
+
+func draw(w, h k, c []c) {
+	b := mkBM(toBM(w, h, c))
+	d := winCreateCompatibleDC(0)
+	xif(d == 0, "create compatible dc")
+	defer winDeleteDC(d)
+	o := winSelectObject(d, b.m)
+	xif(o == 0, "select object")
+	defer winSelectObject(d, o)
+	xif(!winBitBlt(winGetDC(0), 0, 0, b.w, b.h, d, 0, 0, 0x00CC0020), "bitblt")
+	winDeleteObject(b.m)
+	winGlobalUnlock(b.p)
+	winGlobalFree(b.p)
+	b.p, b.m = 0, 0
+}
+func toBM(w, h k, c []c) (r uintptr) {
+	var bi wBM5
+	bi.s = k(unsafe.Sizeof(bi))
+	bi.w, bi.h, bi.p, bi.c, bi.m, bi.x, bi.y = i(w), -i(h), 1, 32, 3, 3780, 3780
+	bi.r, bi.g, bi.b, bi.a = 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000
+	d := winGetDC(0)
+	defer winReleaseDC(0, d)
+	var lpBits unsafe.Pointer
+	r = winCreateDIBSection(d, &bi.wBI, 0, &lpBits, 0, 0)
+	switch r {
+	case 0, 87:
+		panic("CreateDIBSection failed")
+	}
+	a := (*[1 << 30]byte)(unsafe.Pointer(lpBits))
+	copy(a[:], c)
+	return r
+}
+func mkBM(h uintptr) *bitmap {
+	var d wDS
+	xif(winGetObject(h, unsafe.Sizeof(d), unsafe.Pointer(&d)) == 0, "getobject")
+	b := &d.h
+	s, p := uintptr(unsafe.Sizeof(*b)), uintptr(i(b.c)*b.w*b.h)/8
+	c := winGlobalAlloc(0x0042, uintptr(s+p))
+	dest := winGlobalLock(c)
+	defer winGlobalUnlock(c)
+	src := unsafe.Pointer(&d.h)
+	winMoveMemory(dest, src, s)
+	dest = unsafe.Pointer(uintptr(dest) + s)
+	src = d.b.h
+	winMoveMemory(dest, src, p)
+	return &bitmap{m: h, p: c, w: b.w, h: b.h}
+}
+func xif(c bool, e string) {
+	if c {
+		panic(e)
+	}
 }
 
 var (
 	libuser32          = syscall.NewLazyDLL("user32.dll")
 	libgdi32           = syscall.NewLazyDLL("gdi32.dll")
 	libkernel32        = syscall.NewLazyDLL("kernel32.dll")
-	getDC              = libuser32.NewProc("GetDC")
-	releaseDC          = libuser32.NewProc("ReleaseDC")
-	createCompatibleDC = libgdi32.NewProc("CreateCompatibleDC")
-	createDIBSection   = libgdi32.NewProc("CreateDIBSection")
-	deleteDC           = libgdi32.NewProc("DeleteDC")
-	selectObject       = libgdi32.NewProc("SelectObject")
-	bitBlt             = libgdi32.NewProc("BitBlt")
-	deleteObject       = libgdi32.NewProc("DeleteObject")
 	globalAlloc        = libkernel32.NewProc("GlobalAlloc")
 	globalFree         = libkernel32.NewProc("GlobalFree")
 	globalLock         = libkernel32.NewProc("GlobalLock")
 	globalUnlock       = libkernel32.NewProc("GlobalUnlock")
-	getObject          = libgdi32.NewProc("GetObjectW")
 	moveMemory         = libkernel32.NewProc("RtlMoveMemory")
+	getDC              = libuser32.NewProc("GetDC")
+	releaseDC          = libuser32.NewProc("ReleaseDC")
+	deleteDC           = libgdi32.NewProc("DeleteDC")
+	createCompatibleDC = libgdi32.NewProc("CreateCompatibleDC")
+	createDIBSection   = libgdi32.NewProc("CreateDIBSection")
+	selectObject       = libgdi32.NewProc("SelectObject")
+	deleteObject       = libgdi32.NewProc("DeleteObject")
+	getObject          = libgdi32.NewProc("GetObjectW")
+	bitBlt             = libgdi32.NewProc("BitBlt")
 )
 
-func winGetDC(hWnd winHWND) winHDC {
-	ret, _, _ := syscall.Syscall(getDC.Addr(), 1, uintptr(hWnd), 0, 0)
-	return winHDC(ret)
+func winGlobalAlloc(u uint32, d uintptr) (r uintptr) {
+	r, _, _ = syscall.Syscall(globalAlloc.Addr(), 2, uintptr(u), d, 0)
+	return
 }
-func winReleaseDC(hWnd winHWND, hDC winHDC) bool {
-	ret, _, _ := syscall.Syscall(releaseDC.Addr(), 2, uintptr(hWnd), uintptr(hDC), 0)
-	return ret != 0
+func winGlobalFree(h uintptr) (r uintptr) {
+	r, _, _ = syscall.Syscall(globalFree.Addr(), 1, h, 0, 0)
+	return
 }
-func winCreateCompatibleDC(hdc winHDC) winHDC {
-	ret, _, _ := syscall.Syscall(createCompatibleDC.Addr(), 1, uintptr(hdc), 0, 0)
-	return winHDC(ret)
+func winGlobalLock(h uintptr) unsafe.Pointer {
+	r, _, _ := syscall.Syscall(globalLock.Addr(), 1, h, 0, 0)
+	return unsafe.Pointer(r)
 }
-func winCreateDIBSection(hdc winHDC, pbmih *winBITMAPINFOHEADER, iUsage uint32, ppvBits *unsafe.Pointer, hSection winHANDLE, dwOffset uint32) winHBITMAP {
-	ret, _, _ := syscall.Syscall6(createDIBSection.Addr(), 6, uintptr(hdc), uintptr(unsafe.Pointer(pbmih)), uintptr(iUsage), uintptr(unsafe.Pointer(ppvBits)), uintptr(hSection), uintptr(dwOffset))
-	return winHBITMAP(ret)
+func winGlobalUnlock(h uintptr) bool {
+	r, _, _ := syscall.Syscall(globalUnlock.Addr(), 1, h, 0, 0)
+	return r != 0
 }
-
-func winDeleteDC(hdc winHDC) bool {
-	ret, _, _ := syscall.Syscall(deleteDC.Addr(), 1, uintptr(hdc), 0, 0)
-	return ret != 0
+func winMoveMemory(d, s unsafe.Pointer, l uintptr) {
+	syscall.Syscall(moveMemory.Addr(), 3, uintptr(unsafe.Pointer(d)), uintptr(s), l)
 }
-func winSelectObject(hdc winHDC, hgdiobj winHGDIOBJ) winHGDIOBJ {
-	ret, _, _ := syscall.Syscall(selectObject.Addr(), 2, uintptr(hdc), uintptr(hgdiobj), 0)
-	return winHGDIOBJ(ret)
+func winGetDC(h uintptr) (r uintptr) {
+	r, _, _ = syscall.Syscall(getDC.Addr(), 1, h, 0, 0)
+	return
 }
-func winBitBlt(hdcDest winHDC, nXDest, nYDest, nWidth, nHeight int32, hdcSrc winHDC, nXSrc, nYSrc int32, dwRop uint32) bool {
-	ret, _, _ := syscall.Syscall9(bitBlt.Addr(), 9, uintptr(hdcDest), uintptr(nXDest), uintptr(nYDest), uintptr(nWidth), uintptr(nHeight), uintptr(hdcSrc), uintptr(nXSrc), uintptr(nYSrc), uintptr(dwRop))
-	return ret != 0
+func winReleaseDC(w uintptr, d uintptr) bool {
+	r, _, _ := syscall.Syscall(releaseDC.Addr(), 2, w, d, 0)
+	return r != 0
 }
-func winDeleteObject(hObject winHGDIOBJ) bool {
-	ret, _, _ := syscall.Syscall(deleteObject.Addr(), 1, uintptr(hObject), 0, 0)
-	return ret != 0
+func winDeleteDC(h uintptr) bool {
+	r, _, _ := syscall.Syscall(deleteDC.Addr(), 1, h, 0, 0)
+	return r != 0
 }
-func winGlobalAlloc(uFlags uint32, dwBytes uintptr) winHGLOBAL {
-	ret, _, _ := syscall.Syscall(globalAlloc.Addr(), 2, uintptr(uFlags), dwBytes, 0)
-	return winHGLOBAL(ret)
+func winCreateCompatibleDC(h uintptr) (r uintptr) {
+	r, _, _ = syscall.Syscall(createCompatibleDC.Addr(), 1, h, 0, 0)
+	return
 }
-func winGlobalFree(hMem winHGLOBAL) winHGLOBAL {
-	ret, _, _ := syscall.Syscall(globalFree.Addr(), 1, uintptr(hMem), 0, 0)
-	return winHGLOBAL(ret)
+func winCreateDIBSection(h uintptr, p *wBI, u uint32, b *unsafe.Pointer, s uintptr, o uint32) (r uintptr) {
+	r, _, _ = syscall.Syscall6(createDIBSection.Addr(), 6, h, uintptr(unsafe.Pointer(p)), uintptr(u), uintptr(unsafe.Pointer(b)), s, uintptr(o))
+	return
 }
-func winGlobalLock(hMem winHGLOBAL) unsafe.Pointer {
-	ret, _, _ := syscall.Syscall(globalLock.Addr(), 1, uintptr(hMem), 0, 0)
-	return unsafe.Pointer(ret)
+func winSelectObject(h uintptr, o uintptr) (r uintptr) {
+	r, _, _ = syscall.Syscall(selectObject.Addr(), 2, h, o, 0)
+	return
 }
-func winGlobalUnlock(hMem winHGLOBAL) bool {
-	ret, _, _ := syscall.Syscall(globalUnlock.Addr(), 1, uintptr(hMem), 0, 0)
-	return ret != 0
+func winDeleteObject(o uintptr) bool {
+	r, _, _ := syscall.Syscall(deleteObject.Addr(), 1, o, 0, 0)
+	return r != 0
 }
-func winGetObject(hgdiobj winHGDIOBJ, cbBuffer uintptr, lpvObject unsafe.Pointer) int32 {
-	ret, _, _ := syscall.Syscall(getObject.Addr(), 3, uintptr(hgdiobj), uintptr(cbBuffer), uintptr(lpvObject))
-	return int32(ret)
+func winGetObject(g uintptr, b uintptr, o unsafe.Pointer) int32 {
+	r, _, _ := syscall.Syscall(getObject.Addr(), 3, g, b, uintptr(o))
+	return int32(r)
 }
-func winMoveMemory(destination, source unsafe.Pointer, length uintptr) {
-	syscall.Syscall(moveMemory.Addr(), 3, uintptr(unsafe.Pointer(destination)), uintptr(source), uintptr(length))
-}
-
-type Bitmap struct {
-	hBmp       winHBITMAP
-	hPackedDIB winHGLOBAL
-	w, h       int32
-}
-
-func bitmap(w, h k, b []c) *Bitmap {
-	return newBitmapFromHBITMAP(hBitmapFromImage(w, h, b))
-}
-func (bmp *Bitmap) draw() {
-	hdc := winCreateCompatibleDC(0)
-	xif(hdc == 0, "create compatible dc")
-	defer winDeleteDC(hdc)
-	hBmpOld := winSelectObject(hdc, winHGDIOBJ(bmp.hBmp))
-	xif(hBmpOld == 0, "select object")
-	defer winSelectObject(hdc, hBmpOld)
-	xif(!winBitBlt(winGetDC(0), 0, 0, bmp.w, bmp.h, hdc, 0, 0, 0x00CC0020), "bitblt")
-	winDeleteObject(winHGDIOBJ(bmp.hBmp))
-	winGlobalUnlock(bmp.hPackedDIB)
-	winGlobalFree(bmp.hPackedDIB)
-	bmp.hPackedDIB = 0
-	bmp.hBmp = 0
-}
-func hBitmapFromImage(w, h k, b []c) winHBITMAP {
-	var bi winBITMAPV5HEADER
-	bi.BiSize = uint32(unsafe.Sizeof(bi))
-	bi.BiWidth = int32(w)
-	bi.BiHeight = -int32(h)
-	bi.BiPlanes = 1
-	bi.BiBitCount = 32
-	bi.BiCompression = 3
-	bi.BiXPelsPerMeter = 3780 // 96 dpi * 39.37 inches per meter
-	bi.BiYPelsPerMeter = 3780
-	bi.BV4RedMask = 0x00FF0000
-	bi.BV4GreenMask = 0x0000FF00
-	bi.BV4BlueMask = 0x000000FF
-	bi.BV4AlphaMask = 0xFF000000
-	hdc := winGetDC(0)
-	defer winReleaseDC(0, hdc)
-	var lpBits unsafe.Pointer
-	hBitmap := winCreateDIBSection(hdc, &bi.winBITMAPINFOHEADER, 0, &lpBits, 0, 0)
-	switch hBitmap {
-	case 0, 87:
-		panic("CreateDIBSection failed")
-	}
-	bitmap_array := (*[1 << 30]byte)(unsafe.Pointer(lpBits))
-	copy(bitmap_array[:], b)
-	/* TODO: bits must be swapped and a set to 255
-	i := 0
-	for y := im.Bounds().Min.Y; y != im.Bounds().Max.Y; y++ {
-		for x := im.Bounds().Min.X; x != im.Bounds().Max.X; x++ {
-			r, g, b, a := im.At(x, y).RGBA()
-			bitmap_array[i+3] = byte(a >> 8)
-			bitmap_array[i+2] = byte(r >> 8)
-			bitmap_array[i+1] = byte(g >> 8)
-			bitmap_array[i+0] = byte(b >> 8)
-			i += 4
-		}
-	}
-	*/
-	return hBitmap
-}
-func newBitmapFromHBITMAP(hBmp winHBITMAP) (bmp *Bitmap) {
-	var dib winDIBSECTION
-	xif(winGetObject(winHGDIOBJ(hBmp), unsafe.Sizeof(dib), unsafe.Pointer(&dib)) == 0, "getobject")
-	bmih := &dib.DsBmih
-	bmihSize := uintptr(unsafe.Sizeof(*bmih))
-	pixelsSize := uintptr(int32(bmih.BiBitCount)*bmih.BiWidth*bmih.BiHeight) / 8
-	totalSize := uintptr(bmihSize + pixelsSize)
-	hPackedDIB := winGlobalAlloc(0x0042, totalSize)
-	dest := winGlobalLock(hPackedDIB)
-	defer winGlobalUnlock(hPackedDIB)
-	src := unsafe.Pointer(&dib.DsBmih)
-	winMoveMemory(dest, src, bmihSize)
-	dest = unsafe.Pointer(uintptr(dest) + bmihSize)
-	src = dib.DsBm.BmBits
-	winMoveMemory(dest, src, pixelsSize)
-	return &Bitmap{hBmp: hBmp, hPackedDIB: hPackedDIB, w: bmih.BiWidth, h: bmih.BiHeight}
-}
-func xif(c bool, e string) {
-	if c {
-		panic(e)
-	}
+func winBitBlt(d uintptr, x, y, w, h int32, s uintptr, xs, ys int32, o uint32) bool {
+	r, _, _ := syscall.Syscall9(bitBlt.Addr(), 9, d, uintptr(x), uintptr(y), uintptr(w), uintptr(h), s, uintptr(xs), uintptr(ys), uintptr(o))
+	return r != 0
 }
