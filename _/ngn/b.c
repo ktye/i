e73ae9ab3160e415812a5b19e70e903f087879c7
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2020-01-12 12:13:18 +0100

    ngn/k2

--- _/ngn/b.c
+++ _/ngn/b.c
@@ -1,3 +1,14 @@
-int add(int x, int y) {
-	return x+y;
+extern unsigned char __heap_base;
+
+unsigned int b = (unsigned int)&__heap_base;
+void* grw(int n) {
+	unsigned int r = b;
+	b += n;
+	return (void *)r;
 }
+
+
+// https://dassur.ma/things/c-to-webassembly/
+// scroll way down to "LLVM's memory model"
+
+// https://stackoverflow.com/questions/58252467/heap-base-seems-to-be-missing-in-clang-9-0-0-is-there-a-replacement
