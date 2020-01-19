1ad925b47698485116ee6cb50761b68252f3265e
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2020-01-19 16:22:09 +0100

    w0 runs

--- _/w/w.go
+++ _/w/w.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"io"
@@ -10,42 +11,47 @@ import (
 )
 
 type c = byte
-
-const (
-	sFname = iota
-	sRety
-	sArgs
-	sLocl
-	sBody
-	sNewl
-)
-
 type T c
-
-const (
-	I = T(0x07)
-	F = T(0x7c)
-)
-
-// name:I:IIF::body..
-type fn struct {
+type fn struct { // name:I:IIF::body..
 	name string
 	rety T
 	args []T
 	locl []T
 	sign int
+	bytes.Buffer
 }
 
-var typs = map[b]T{'I': 0x07, 'F': 0x7c}
+const (
+	I = T(0x7f)
+	F = T(0x7c)
+)
+const (
+	sFnam = iota
+	sRety
+	sArgs
+	sLocl
+	sBody
+	sCmnt
+	sNewl
+)
 
-type module []f
+var typs = map[c]T{'I': I, 'F': F}
+
+type module []fn
 
 func main() {
 	var html bool
 	flag.BoolVar(&html, "html", false, "html output")
 	flag.Parse()
-	rd := bufio.NewReader(os.Stdin)
-	state := sFname
+	b := run(os.Stdin)
+	if html {
+		b = page(b)
+	}
+	os.Stdout.Write(b)
+}
+func run(r io.Reader) []c {
+	rd := bufio.NewReader(r)
+	state := sFnam
 	line, char, hi := 1, 0, true
 	err := func(s string) { fatal(fmt.Errorf("%d:%d: %s", line, char, s)) }
 	var m module
@@ -54,16 +60,10 @@ func main() {
 	for {
 		b, e := rd.ReadByte()
 		if e == io.EOF {
-			if state != sFname {
-				err("parse trailing")
+			if f.name != "" {
+				m = append(m, f)
 			}
-			wasm := m.emit()
-			if html {
-				page(wasm)
-			} else {
-				os.Stdout.Write(wasm)
-			}
-			return
+			return m.emit()
 		}
 		char++
 		if b == '\n' {
@@ -71,11 +71,11 @@ func main() {
 		}
 		fatal(e)
 		switch state {
-		case sFname:
+		case sFnam:
 			if craZ(b) {
 				f.name += string(b)
 			} else if b == ':' {
-				state++
+				state = sRety
 			} else {
 				err("parse function name")
 			}
@@ -86,7 +86,7 @@ func main() {
 					panic("parse return type")
 				}
 			} else if b == ':' {
-				state++
+				state = sArgs
 			} else {
 				err("parse return type")
 			}
@@ -96,7 +96,7 @@ func main() {
 			} else if t != 0 {
 				f.args = append(f.args, t)
 			} else if b == ':' {
-				state++
+				state = sLocl
 			} else {
 				err("parse args")
 			}
@@ -104,13 +104,13 @@ func main() {
 			if t := typs[b]; t != 0 {
 				f.args = append(f.locl, t)
 			} else if b == ':' {
-				state++
+				state = sBody
 			} else {
 				err("parse locals")
 			}
 		case sBody:
 			if b == '/' && hi {
-				state == sCmnt
+				state = sCmnt
 			} else if (b == ' ' || b == '\t') && hi {
 				continue
 			} else if (b == '\n') && hi {
@@ -120,6 +120,8 @@ func main() {
 					p, hi = xtoc(b)<<4, false
 				} else {
 					p, hi = p|xtoc(b), true
+					f.WriteByte(p)
+					p = 0
 				}
 			} else {
 				err("parse body")
@@ -129,29 +131,28 @@ func main() {
 				state = sNewl
 			}
 		case sNewl:
-			if b == ' ' || b == '\t' {
-				state--
+			if b == '\n' {
+				state = sCmnt
+			} else if b == ' ' || b == '\t' {
+				if f.name == "" {
+					err("parse name")
+				}
+				state = sBody
 			} else {
 				m = append(m, f)
 				f = fn{}
-				state = fname
+				state = sFnam
 			}
 		default:
 			err("internal parse state")
 		}
 	}
 }
-func fatal(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 func hxb(x c) (c, c) { h := "0123456789abcdef"; return h[x>>4], h[x&0x0F] }
-
-func cr09(c c) bool { return c >= '0' && c <= '9' }
-func craZ(c c) bool { return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') }
-func cr0Z(c c) bool { return cr09(c) || craZ(c) }
-func crHx(c c) bool { return cr09(c) || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F') }
+func cr09(c c) bool  { return c >= '0' && c <= '9' }
+func craZ(c c) bool  { return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') }
+func cr0Z(c c) bool  { return cr09(c) || craZ(c) }
+func crHx(c c) bool  { return cr09(c) || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F') }
 func xtoc(x c) c {
 	switch {
 	case x < ':':
@@ -162,53 +163,107 @@ func xtoc(x c) c {
 		return 10 + x - 'a'
 	}
 }
+func fatal(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func (m module) emit() []c {
-	var b bytes.Buffer
-	b.Write([]c{0, 0x64, 0x73, 0x6d, 1, 0, 0, 0}) // header
-
-	ts := section{t: 1} // type section
+	o := bytes.NewBuffer([]c{0, 0x61, 0x73, 0x6d, 1, 0, 0, 0}) // header
+	// type section(1: function signatures)
+	sec := NewSection(1)
 	sigs, sigv := make(map[string]int), make([]string, 0)
 	for i, f := range m {
 		s := string(f.sig())
 		if n, o := sigs[s]; o == false {
-			n = len(sigs[s])
+			n = len(sigs)
 			sigs[s] = n
 			sigv = append(sigv, s)
 			m[i].sign = n
 		}
 	}
-	ts.Buffer.Write(lebu(len(sigv)))
-	for i, s := range sigv {
-		ts.Buffer.WriteString(s)
+	sec.cat(lebu(len(sigv)))
+	for _, s := range sigv {
+		sec.cat([]c(s))
 	}
-	ts.add(b)
-
-	//todo...
+	sec.out(o)
+	// no import section(2)
+	// function section(3: function signature indexes)
+	sec = NewSection(3)
+	for _, f := range m {
+		sec.cat(lebu(len(m)))
+		sec.cat(lebu(sigs[string(f.sig())]))
+	}
+	sec.out(o)
+	// no table section(4)
+	// linear memory section(5)
+	sec = NewSection(5)
+	sec.cat([]c{1, 0, 1}) // 1 initial memory segment, unshared, size 1 block
+	sec.out(o)
+	// no global section(6)
+	// export section(7)
+	sec = NewSection(7)
+	sec.cat(lebu(len(m))) // number of exports (all)
+	for i, f := range m {
+		sec.cat(lebu(len(f.name)))
+		sec.cat([]c(f.name))
+		sec.cat1(0) // function-export
+		sec.cat(lebu(i))
+	}
+	sec.out(o)
+	// no start section(8)
+	// no element section(9)
+	// code section(10)
+	sec = NewSection(10)
+	sec.cat(lebu(len(m))) // number of functions
+	for _, f := range m {
+		sec.cat(f.code())
+	}
+	sec.out(o)
+	// no data section(11)
+	return o.Bytes()
 }
 
 type section struct {
-	bytes.Buffer
+	t c
+	b []c
+}
+
+func NewSection(t c) section { return section{t: t} }
+func (s *section) cat(b []c) { s.b = append(s.b, b...) }
+func (s *section) cat1(b c)  { s.b = append(s.b, b) }
+func (s *section) out(wt *bytes.Buffer) {
+	var w bytes.Buffer
+	w.WriteByte(s.t)
+	w.Write(lebu(len(s.b)))
+	w.Write(s.b)
+	b := w.Bytes()
+	wt.Write(b)
 }
 
 func (f fn) sig() (r []c) {
 	r = append(r, 0x60)
-	r = append(r, lebu(f.args))
+	r = append(r, lebu(len(f.args))...)
 	for _, t := range f.args {
-		t = append(r, t)
+		r = append(r, c(t))
 	}
 	r = append(r, 1)
-	r = append(r, f.rety)
+	r = append(r, c(f.rety))
 	return r
 }
-
-func (s section) add(w io.Writer) {
-	w.WriteByte(s.t)
-	w.Write(lebu(s.Buffer.Size()))
-	w.Write(s.Buffer.Bytes())
+func (f fn) code() (r []c) {
+	r = append(r, lebu(len(f.locl))...)
+	for _, t := range f.locl {
+		r = append(r, c(t))
+	}
+	r = append(r, f.Bytes()...)
+	r = append(r, 0x0b)
+	return append(lebu(len(r)), r...)
 }
 
-func lebu(b []b, v uint64) []byte { // encode leb128
+func lebu(v int) []c { // encode unsigned leb128
+	var b []c
 	for {
 		c := uint8(v & 0x7f)
 		v >>= 7
@@ -221,10 +276,6 @@ func lebu(b []b, v uint64) []byte { // encode leb128
 		}
 	}
 	return b
-}
-
-func page(w []c) {
-	panic("todo")
 }
 
 /*
@@ -244,3 +295,30 @@ func lebs(b []b, v int64) []b { // encode signed leb128
 	return b
 }
 */
+
+func page(wasm []c) []c {
+	var b bytes.Buffer
+	b.Write([]c(head))
+	b.WriteString(base64.StdEncoding.EncodeToString(wasm))
+	b.Write([]c(tail))
+	return b.Bytes()
+}
+
+const head = `<html>
+<head><meta charset="utf-8"><title>w</title></head><body><script>
+var us = function(s){var r=new Uint8Array(new ArrayBuffer(s.length));for(var i=0;i<s.length;i++)r[i]=s.charCodeAt(i);return r};
+var s = "`
+const tail = `"
+var u = us(atob(s));
+(async() => {
+var r=await WebAssembly.instantiate(u)
+window.k=r.instance.exports
+})()
+// browse to file://../index.html
+</script>
+<pre>
+run wasm from js console, e.g:
+ k.add(1,2)
+</pre>
+</body></html>
+`
--- _/w/w_test.go
+++ _/w/w_test.go
@@ -1,29 +1,28 @@
 package main
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestW(t *testing.T) {
 	testCases := [][2]string{
-		{" .626363 \n636261\n\n", "616263636261"},
+		{"add:I:II::2000 2001 6a\n", "0061736d0100000001070160027f7f017f0302010005030100010707010361646400000a09010700200020016a0b"},
 	}
-	for _, tc := range testCases {
-		in := tc[0]
-		var r []c
-		v := strings.Split(in, "\n")
-		for _, s := range v {
-			r = append(r, run([]c(s)))
-		}
-		if s := string(hex(r)); s != tc[1] {
-			t.Fatalf("expected %q got %q\n", tc[1], s)
+	for n, tc := range testCases {
+		i, e := tc[0], tc[1]
+		o := string(hex(run(bytes.NewReader([]c(i)))))
+		if o != e {
+			t.Fatalf("%d: expected/got:\n%q\n%q\n", n, e, o)
 		}
 	}
 }
-func hex(b []c) (r []c) {
-	r = make(2 * len(b))
-	for i, z := range b {
-		r[2*i], r[2*i+1] = hxb(z)
+func hex(a []c) []c {
+	var r bytes.Buffer
+	for _, b := range a {
+		hi, lo := hxb(b)
+		r.WriteByte(hi)
+		r.WriteByte(lo)
 	}
-	return r
+	return r.Bytes()
 }
