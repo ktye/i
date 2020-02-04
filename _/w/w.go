f27c83541a157e9e906edb9e17d1c0385810f29d
U-DDA01\csd80117 <ktye@users.noreply.github.com>
2020-02-04 18:23:47 +0100

    w !(trap)

--- _/w/w.go
+++ _/w/w.go
@@ -853,9 +853,7 @@ func getop(tab map[s]code, op s, t T) (r c) {
 	default:
 		panic("type(" + op + ")")
 	}
-	if r == 0xff {
-		r = 0
-	} else if r == 0 {
+	if r == 0 {
 		panic("type(" + op + ")")
 	}
 	return r
@@ -1261,8 +1259,10 @@ func (v brif) bytes() []c { return append(v.x().bytes(), 0x0d, 0x01) } // break 
 func (v brif) cstr() s    { return jn("if(", cstring(v.x()), ")break;") }
 func (v brif) gstr() s    { return jn("if ", gstring(v.x()), "{break}") }
 func (v opx) rt() T       { return 0 }
-func (v opx) valid() s    { return "nonapplied operator" }
-func (v opx) bytes() []c  { return nil }
+func (v opx) valid() s    { return ifex(s(v) != "!", "nonapplied operator") }
+func (v opx) bytes() []c  { return []c{0x00} }
+func (v opx) cstr() s     { return "trap();" }
+func (v opx) gstr() s     { return `panic("trap")` }
 func (v pos) rt() T       { return 0 }
 func (v pos) valid() s    { return "position(dummy expr)" }
 func (v pos) bytes() []c  { return nil }
@@ -1325,7 +1325,6 @@ var v1Tab = map[s]code{
 	"*": code{0x67, 0x79, 0, "__builtin_clz;__builtin_clzll;", "Ubits.LeadingZeros32;Ubits.LeadingZeros64;"},   // clz
 	"|": code{0x68, 0x79, 0, "__builtin_ctz;__builtin_ctzll;", "Ubits.TrailingZeros32;Ubits;TrailingZeros64;"}, // ctz
 	"%": code{0, 0, 0x9f, "sqrt", "math.Sqrt"},                                                                 // sqr
-	"!": code{0xff, 0xff, 0xff, "trap", "panic"},                                                               // 0xff → 0x00 in getop
 }
 var v2Tab = map[s]code{
 	`+`:   code{0x6a, 0x7c, 0xa0, "+", "+"},     // add
