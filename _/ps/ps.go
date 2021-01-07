ff982397e3194e721ddeea6eb73181132723a0a4
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2021-01-07 15:38:35 +0100

    ps.go

--- _/ps/ps.go
+++ _/ps/ps.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 	"math/cmplx"
+	"math/rand"
 	"os"
 	"reflect"
 	"runtime"
@@ -46,7 +47,7 @@ type (
 	Integer    int
 	Real       float64
 	Complex    complex128
-	Mark       bool
+	Mark       string
 	Name       string
 	Null       bool
 	Operator   func(*Interpreter)
@@ -79,6 +80,8 @@ func (z Complex) String() string {
 	}
 	return fmt.Sprintf("%v@%s", r, ang)
 }
+func (m Mark) Exec(i *Interpreter)       { i.Push(m) }
+func (m Mark) String() string            { return string(m) }
 func (d Dictionary) Exec(i *Interpreter) { i.Push(d) }
 func (d Dictionary) String() string {
 	var b strings.Builder
@@ -94,6 +97,136 @@ func (n Name) String() string          { return string(n) }
 func (o Operator) Exec(i *Interpreter) { o(i) }
 func (o Operator) String() string      { return runtime.FuncForPC(reflect.ValueOf(o).Pointer()).Name() }
 
+// stack operators
+func pop(i *Interpreter) { _ = i.Pop() }
+func exch(i *Interpreter) {
+	n := len(i.v.stack)
+	i.v.stack[n-1], i.v.stack[n-2] = i.v.stack[n-2], i.v.stack[n-1]
+}
+func dup(i *Interpreter) { x := i.Pop(); i.Push(x); i.Push(x) }
+func _copy(i *Interpreter) {
+	n := i.Pop().(Integer)
+	if n < 0 {
+		i.err("range")
+	}
+	o := len(i.v.stack) - int(n)
+	for k := 0; k < int(n); k++ {
+		i.Push(i.v.stack[o+k])
+	}
+}
+func index(i *Interpreter) { cvi(i); i.Push(i.v.stack[len(i.v.stack)-int(i.Pop().(Integer))-1]) }
+func roll(i *Interpreter) {
+	k := int(i.Pop().(Integer))
+	n := int(i.Pop().(Integer))
+	if n < 0 {
+		i.err("range")
+	}
+	k %= n
+	v := make([]Value, n)
+	copy(v, i.v.stack[len(i.v.stack)-n:])
+	i.v.stack = i.v.stack[:len(i.v.stack)-n]
+	for j := 0; j < n; j++ {
+		i.Push(v[(n+j-k)%n])
+	}
+}
+func clear(i *Interpreter) { i.v.stack = i.v.stack[:0] }
+func count(i *Interpreter) { i.Push(Integer(len(i.v.stack))) }
+func mark(i *Interpreter)  { i.Push(Mark("mark")) }
+func cleartomark(i *Interpreter) {
+	counttomark(i)
+	n := int(i.Pop().(Integer))
+	i.v.stack = i.v.stack[:len(i.v.stack)-n-1]
+}
+func counttomark(i *Interpreter) {
+	l := len(i.v.stack)
+	for k := 0; k < l; k++ {
+		if _, ok := i.v.stack[l-k-1].(Mark); ok {
+			i.Push(Integer(k))
+			return
+		}
+	}
+	i.err("unmatchedmark")
+}
+
+// arithmetic operators
+func add(i *Interpreter) {
+	numOp2(i, 1, 0, func(x, y int) int { return x + y }, func(x, y float64) float64 { return x + y }, func(x, y complex128) complex128 { return x + y })
+}
+func sub(i *Interpreter) {
+	numOp2(i, 1, 0, func(x, y int) int { return x - y }, func(x, y float64) float64 { return x - y }, func(x, y complex128) complex128 { return x - y })
+}
+func mul(i *Interpreter) {
+	numOp2(i, 1, 0, func(x, y int) int { return x * y }, func(x, y float64) float64 { return x * y }, func(x, y complex128) complex128 { return x * y })
+}
+func div(i *Interpreter) {
+	numOp2(i, 2, 0, func(x, y int) int { return x / y }, func(x, y float64) float64 { return x / y }, func(x, y complex128) complex128 { return x / y })
+}
+func idiv(i *Interpreter) {
+	numOp2(i, 0, 1, func(x, y int) int { return x / y }, nil, nil)
+}
+func mod(i *Interpreter) {
+	numOp2(i, 0, 1, func(x, y int) int { return x % y }, func(x, y float64) float64 { return math.Mod(x, y) }, nil)
+}
+func abs(i *Interpreter) {
+	x := i.Pop()
+	switch v := x.(type) {
+	case Integer:
+		if v < 0 {
+			v = -v
+		}
+		i.Push(v)
+	case Real:
+		i.Push(Real(math.Abs(float64(v))))
+	case Complex:
+		i.Push(Real(cmplx.Abs(complex128(v))))
+	default:
+		i.err("type")
+	}
+}
+func neg(i *Interpreter) {
+	numOp1(i, 0, 0, func(x int) int { return -x }, func(x float64) float64 { return -x }, func(x complex128) complex128 { return -x })
+}
+func ceiling(i *Interpreter) {
+	numOp1(i, 0, 2, func(x int) int { return x }, func(x float64) float64 { return math.Ceil(x) }, nil)
+}
+func floor(i *Interpreter) {
+	numOp1(i, 0, 2, func(x int) int { return x }, func(x float64) float64 { return math.Floor(x) }, nil)
+}
+func round(i *Interpreter) {
+	numOp1(i, 0, 2, func(x int) int { return x }, func(x float64) float64 { return math.Round(x) }, nil)
+}
+func truncate(i *Interpreter) {
+	numOp1(i, 0, 2, func(x int) int { return x }, func(x float64) float64 { return math.Trunc(x) }, nil)
+}
+func sqrt(i *Interpreter) {
+	numOp1(i, 2, 2, nil, func(x float64) float64 { return math.Sqrt(x) }, nil)
+}
+func atan(i *Interpreter) {
+	numOp2(i, 2, 2, nil, func(x, y float64) float64 { return math.Atan2(x, y) }, nil)
+}
+func cos(i *Interpreter) {
+	numOp1(i, 2, 2, nil, func(x float64) float64 { return math.Cos(x) }, nil)
+}
+func sin(i *Interpreter) {
+	numOp1(i, 2, 2, nil, func(x float64) float64 { return math.Sin(x) }, nil)
+}
+func exp(i *Interpreter) { // pow
+	numOp2(i, 2, 2, nil, func(x, y float64) float64 { return math.Pow(x, y) }, nil)
+}
+func ln(i *Interpreter) { // log base e
+	numOp1(i, 2, 2, nil, func(x float64) float64 { return math.Log(x) }, nil)
+}
+func log(i *Interpreter) { // log base 10
+	numOp1(i, 2, 2, nil, func(x float64) float64 { return math.Log10(x) }, nil)
+}
+func _rand(i *Interpreter) { i.Push(Integer(rand.Int())) }
+func srand(i *Interpreter) {
+	x := i.Pop()
+	if numType(x) != 1 {
+		i.err("type")
+	}
+	rand.Seed(int64(x.(Integer)))
+}
 func numType(v Value) int {
 	switch v.(type) {
 	case Integer:
@@ -114,18 +247,46 @@ func uptype(v Value, t int) (Value, int) {
 	}
 	panic("unreachable")
 }
-func numOp2(i *Interpreter, fi func(x, y int) int, fr func(x, y float64) float64, fz func(x, y complex128) complex128) {
+func numOp1(i *Interpreter, minType, maxType int, fi func(x int) int, fr func(x float64) float64, fz func(x complex128) complex128) {
+	x := i.Pop()
+	xt := numType(x)
+	if xt == 0 {
+		i.err("type")
+	}
+	for xt < minType {
+		x, xt = uptype(x, xt)
+	}
+	if maxType > 0 && xt > maxType {
+		i.err("type")
+	}
+	switch xt {
+	case 1:
+		i.Push(Integer(fi(int(x.(Integer)))))
+	case 2:
+		i.Push(Real(fr(float64(x.(Real)))))
+	case 3:
+		i.Push(Complex(fz(complex128(x.(Complex)))))
+	}
+}
+func numOp2(i *Interpreter, minType, maxType int, fi func(x, y int) int, fr func(x, y float64) float64, fz func(x, y complex128) complex128) {
 	y := i.Pop()
 	x := i.Pop()
 	xt, yt := numType(x), numType(y)
 	if xt*yt == 0 {
-		i.err("not-numeric")
+		i.err("type")
 	}
 	for xt < yt {
 		x, xt = uptype(x, xt)
 	}
 	for yt < xt {
 		y, yt = uptype(y, yt)
+	}
+	for xt < minType {
+		x, xt = uptype(x, xt)
+		y, yt = uptype(y, yt)
+	}
+	if maxType > 0 && xt > maxType {
+		i.err("type")
 	}
 	switch xt {
 	case 1:
@@ -136,9 +297,19 @@ func numOp2(i *Interpreter, fi func(x, y int) int, fr func(x, y float64) float64
 		i.Push(Complex(fz(complex128(x.(Complex)), complex128(y.(Complex)))))
 	}
 }
-func add(i *Interpreter) {
-	numOp2(i, func(x, y int) int { return x + y }, func(x, y float64) float64 { return x + y }, func(x, y complex128) complex128 { return x + y })
+
+func cvi(i *Interpreter) {
+	x := i.Pop()
+	switch v := x.(type) {
+	case Integer:
+		i.Push(x)
+	case Real:
+		i.Push(Integer(v))
+	default:
+		i.err("type")
+	}
 }
+
 func (i *Interpreter) Run(s string) {
 	token, b := []rune{}, []rune(s)
 	for {
@@ -231,14 +402,46 @@ func (i *Interpreter) where(v Value) Dictionary {
 	return nil
 }
 func mkBuiltins() Dictionary {
-	d := make(Dictionary)
-	d[Name("stack")] = Operator(pstack) // we only have pstack
-	d[Name("pstack")] = Operator(pstack)
-	d[Name("pop")] = Operator(pop)
-	d[Name("exch")] = Operator(exch)
-	d[Name("=")] = Operator(_print)
-	d[Name("==")] = Operator(_print)
-	return d
+	return Dictionary{
+		Name("pop"):         Operator(pop),
+		Name("exch"):        Operator(exch),
+		Name("dup"):         Operator(dup),
+		Name("copy"):        Operator(_copy),
+		Name("index"):       Operator(index),
+		Name("roll"):        Operator(roll),
+		Name("clear"):       Operator(clear),
+		Name("count"):       Operator(count),
+		Name("mark"):        Operator(mark),
+		Name("cleartomark"): Operator(cleartomark),
+		Name("counttomark"): Operator(counttomark),
+
+		Name("add"):      Operator(add),
+		Name("div"):      Operator(div),
+		Name("idiv"):     Operator(idiv),
+		Name("mod"):      Operator(mod),
+		Name("mul"):      Operator(mul),
+		Name("sub"):      Operator(sub),
+		Name("abs"):      Operator(abs),
+		Name("neg"):      Operator(neg),
+		Name("ceiling"):  Operator(ceiling),
+		Name("floor"):    Operator(floor),
+		Name("round"):    Operator(round),
+		Name("truncate"): Operator(truncate),
+		Name("sqrt"):     Operator(sqrt),
+		Name("atan"):     Operator(atan),
+		Name("cos"):      Operator(cos),
+		Name("sin"):      Operator(sin),
+		Name("exp"):      Operator(exp),
+		Name("ln"):       Operator(ln),
+		Name("log"):      Operator(log),
+		Name("rand"):     Operator(_rand),
+		Name("srand"):    Operator(srand),
+
+		Name("stack"):  Operator(pstack), // we only have pstack
+		Name("pstack"): Operator(pstack),
+		Name("="):      Operator(_print),
+		Name("=="):     Operator(_print),
+	}
 }
 func pstack(i *Interpreter) {
 	for n := len(i.v.stack) - 1; n >= 0; n-- {
@@ -246,11 +449,7 @@ func pstack(i *Interpreter) {
 	}
 	fmt.Println() // to separate multiple calls. gs does not do this.
 }
-func exch(i *Interpreter) {
-	n := len(i.v.stack)
-	i.v.stack[n-1], i.v.stack[n-2] = i.v.stack[n-2], i.v.stack[n-1]
-}
-func pop(i *Interpreter)    { _ = i.Pop() }
+
 func _print(i *Interpreter) { v := i.Pop(); fmt.Printf("%s\n", v) }
 func (i *Interpreter) prompt() {
 	if n := len(i.v.stack); n > 0 {
@@ -259,9 +458,14 @@ func (i *Interpreter) prompt() {
 		fmt.Printf("PS>")
 	}
 }
+
 func main() {
 	s := bufio.NewScanner(os.Stdin)
 	i := New()
+	if len(os.Args) > 1 {
+		i.Run(strings.Join(os.Args[1:], " "))
+		return
+	}
 	i.prompt()
 	for s.Scan() {
 		i.Run(s.Text())
