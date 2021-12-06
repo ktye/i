8abc44f9546e2a8059e7d5484ee9fd733c0b3b0c
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2021-12-06 11:36:55 +0100

    ktye header library

--- +/k.c
+++ +/k.c
@@ -1,26 +1,15 @@
 #include<stdio.h>
-
-#define KTYE
-#include"k.h"
+#include"ktye.h"  // ktye/k implementation
 
 // external libraries
 void loadlib(const char*);
 
-// ktye internal api
-int args_;
-char **argv_;
-void **_F;
-void cinit();
-void kinit();
-void doargs();
-void store();
-void try();
-K read();
-int32_t idx(int32_t, int32_t, int32_t);
 
 // c-api implementation
+#define K uint64_t
+void kinit(){ ktye_cinit();ktye_kinit(); }
 K K2(char v, K x, K y) { 
- int32_t p=1+idx(v,288,253);
+ int32_t p=1+ktye_idx(v,288,253);
  return ((K(*)(K,K))_F[64+p])(x,y); 
 }
 void KR(const char *name, void *fp, int arity) {
@@ -31,18 +20,17 @@ void KR(const char *name, void *fp, int arity) {
 int main(int args, char **argv){
  args_=(int32_t)args;
  argv_=argv;
- cinit();
  kinit();
 
  loadlib("lib");
 
- doargs();
+ ktye_doargs();
  printf("ktye/k+\n");
- store();
+ ktye_store();
  while(1){
   printf(" ");
-  K x = read( );
-  try(x);
+  K x = ktye_read( );
+  ktye_try(x);
  }
  return 0;
 }
