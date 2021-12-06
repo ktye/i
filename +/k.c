126df07e11b6ae05d2adbfe32daee1dd032d0af2
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2021-12-06 17:35:30 +0100

    api implementation

--- +/k.c
+++ +/k.c
@@ -1,21 +1,9 @@
 #include<stdio.h>
+#include"k.h"
 #include"ktye.h"  // ktye/k implementation
 
 // external libraries
 void loadlib(const char*);
-
-
-// c-api implementation
-#define K uint64_t
-void kinit(){ ktye_cinit();ktye_kinit(); }
-K K2(char v, K x, K y) { 
- int32_t p=1+ktye_idx(v,288,253);
- return ((K(*)(K,K))_F[64+p])(x,y); 
-}
-void KR(const char *name, void *fp, int arity) {
- //todo
-}
-//todo..
 
 int main(int args, char **argv){
  args_=(int32_t)args;
