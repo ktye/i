bd9ed26b86d1b3594ab6aa86e6b346364fe96eb3
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2022-01-18 16:53:45 +0100

    web

--- web/k.js
+++ web/k.js
@@ -1,5 +1,65 @@
 // k.js is a native javascript module that loads k.wasm and provides the api similar to +/k.h
 
+let K={}  // exported
+let _     // k wasm export object
+
+function lo(x){return Number(BigInt.asUintN(32,x))}         // 32-bit of BigInt serves as the wasm memory index (pointer).
+function us(s){return new TextEncoder("utf-8").encode(s)}
+function su(u){return (u.length)?new TextDecoder("utf-8").decode(u):""}
+//function ku(u){_.memory.buffer
+function dxr(x,r){_.dx(x);return r}
+
+function C(){ return new     Int8Array(_.memory.buffer) }
+function I(){ return new    Int32Array(_.memory.buffer) }
+function J(){ return new BigInt64Array(_.memory.buffer) }
+function F(){ return new  Float64Array(_.memory.buffer) }
+
+// create k atoms
+K.Kc = function(x){ return _.Kc( ("string"===typeof(x)) ? x.charCodeAt(0) : x ) }
+K.Ks = function(x){ return _.sc(K.Kc(x)) }
+K.Ki = function(x){ return _.Ki(x) }
+K.Kf = function(x){ return _.Kf(x) }
+
+// create k vectors
+K.KC = function(x){
+ if("string"===typeof(x))x=us(x);
+ let r=_.mk(18,x.length);C().set(x,lo(r));return r
+}
+K.KS = function(x){
+ let n=x.length
+ var r=_.mk(20,n)
+ var p=I().slice(lo(r))
+ for(let i=0;i<n;i++)p[i]=lo(K.s(x[i]))
+ return r
+}
+K.KI = function(x){
+ x = (x.constructor===Int32Array) ? x : new Int32Array(x)
+ let r=_.mk(19,x.length)
+ I().set(x,lo(r)>>>2)
+ return r
+}
+K.KF = function(x){
+ x = (x.constructor===Float64Array) ? x : new Int32Array(x)
+ let r=_.mk(21,x.length)
+ F().set(x,lo(r)>>>3)
+ return r
+}
+K.KL = function(x){
+ let n=x.length
+ let r=_.mk(23,n)
+ let p=J().slice(lo(r)>>>3)
+ for(let i=0;i<n;i++)p[i]=x[i]
+ return r
+}
+
+K.cK = function(x){ return lo(x) << 24 >> 24 } // signed int8
+ 
+
+K.CK = function(x){ return dxr(x, su(C().slice(lo(x),lo(x)+_.nn(x)))) }
+
+K.iK = function(x){ return lo(x) << 0 } // signed int32
+
+K.IK = function(x){ let p=lo(x)>>>2;return dxr(x,I().slice(p,p+_.nn(x))) }
 
 function reset(){
  
@@ -27,24 +87,19 @@ var env={wasi_unstable:{
  clock_time_get:function(a,b,p){msl();J[p>>>3]=BigInt.asIntN(64, BigInt(Math.floor(1e6*performance.now())));return 0}
 }}
 
-// kinit loads k.wasm then executes f passing k.wasm binary size as an argument.
-//  w(name_string, data_uint8array) is called when k writes to a file, w("", data) is stdout, e.g. terminal. no return value.
-//  r(name_string) returns data_uint8array when k reads from a file. there is no stdin in k.js/wasm.
-function kinit(f,r,w){ 
+K.kinit = function(f,r,w){ // f:onsuccess, r:read, w:write
  let n=0; function binsize(x){n=x.byteLength;return x}
  fetch('k.wasm').then(r=>r.arrayBuffer()).then(r=>WebAssembly.instantiate(binsize(r),env)).then(r=>{
-  let K=r.instance.exports
-  K.kinit();
-
-  K.reset()
+  _=r.instance.exports
+  _.kinit()
+  _.reset()
+  // todo
   // set WIDTH HEIGHT FH FW?
   // ksave()
-  
-
- 
-  f(n);
+  f()
  })
  
 }
 
-export { kinit }
+export { K }
+window.K = K // for browser console
