0454678219acc0461445e79e4b2d955db2ff0943
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2022-05-17 11:33:51 +0200

    go.k: tab

---
+++ x/tests/tab.go
@@ -0,0 +1,18 @@
+package tab
+
+import . "github.com/ktye/wg/module"
+
+func init() {
+
+	Functions(0, f)
+	Functions(1, g)
+	Functions(2, h)
+}
+func f() {
+}
+func g(x int32) int32 {
+	return x
+}
+func h(x int32, y int32) int32 {
+	return (x + y)
+}
