a26c4fa5193942c23e171b88184cdcc11b429c1a
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2022-09-11 22:11:58 +0200

    wa swc cnd rtyp

--- x/tests/swtch.go
+++ x/tests/swtch.go
@@ -4,9 +4,16 @@ func f(x int32) int32 {
 	switch x {
 	case 0:
 		return (1 + x)
-	case 1:
-		return (2 * x)
 	default:
 		return x
 	}
+}
+func g(x int32) int32 {
+	switch x {
+	case 0:
+		return (1 + x)
+	case 1:
+		return x
+	}
+	return 0
 }
