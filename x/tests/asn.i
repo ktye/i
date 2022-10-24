d7aa57a955bbb3674368dff37434f3846ed54405
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2022-10-24 13:23:06 +0200

    ki

--- x/tests/asn.i
+++ x/tests/asn.i
@@ -3,5 +3,5 @@
 
 /functions
 f[i]:{[xi]ri:1+xi;ri}
-g[ii]:{[xi](1+xi;1-xi)}
+g[ii]:{[xi](,1+xi;,1-xi)}
 h[i]:{[xi]`xi`ri:g[xi];ri}
--- x/tests/kitest.sh
+++ x/tests/kitest.sh
@@ -1,4 +1,4 @@
-k ../ki.k -e 'x:kixx@<`"asn.i"' > out1
-k asn.k   -e 'x:`<"\n"/:(`lxy 40 100)@+`T`P`I`S!(T;P;I;S)' > out2
+k ../ki.k -e 'x:kixx@<`"blank.i"' > out1
+k blank.k   -e 'x:`<"\n"/:(`lxy 40 100)@+`T`P`I`S!(T;P;I;S)' > out2
 
 vimdiff out1 out2
