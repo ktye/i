c137e02a3912c80aaf6811f9f27913f53154ed11
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2022-11-04 19:38:39 +0100

    decker

--- _/i2/kui/f/gen.go
+++ _/i2/kui/f/gen.go
@@ -8,15 +8,21 @@
 //
 // go run gen.go 16x32.png psf 16 32 | txt2psf > 16x32.psf
 // go run gen.go 16x32.png psf 16 32 | txt2psf | psf2bdf > 16x32.psf
+//
+// convert to decker fonts: github.com/JohnEarnest/Decker/blob/main/docs/format.md#data-blocks
+// go run gen.go 16x32.png decker 16 32   (to stdout)
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"image"
 	"image/png"
 	"os"
 	"strconv"
 )
+
+type dekfnt map[rune]string
 
 var φ string
 
@@ -27,7 +33,7 @@ func main() {
 	f, err := os.Open(os.Args[1])
 	p(err)
 	φ = os.Args[2]
-	if φ != "k" && φ != "psf" {
+	if φ != "k" && φ != "psf" && φ != "decker" {
 		panic("format")
 	}
 	w, err := strconv.Atoi(os.Args[3])
@@ -45,6 +51,7 @@ func main() {
 	nn := 42 + 26 + 26
 	printp("%%PSF2\nVersion: 0\nFlags: 1\nLength: %d\nWidth: %d\nHeight: %d\n\n", nn, w, h)
 
+	dek := make(dekfnt)
 	chars := []string{"0123456789ABCDEF", ":+-*%&|<>=!~,^#_$?@.0123456789'/\\;`\"(){}[]", "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
 	y, n := 0, 0
 	printk("font:(")
@@ -62,6 +69,7 @@ func main() {
 				}
 				printk("\n %s%s /%c", s, cls, chars[row][col])
 				printp("%%\nBitmap: %s\nUnicode: [00%X];\n", s, c)
+				dek[rune(chars[row][col])] = s
 				n++
 			}
 			nl = "\n "
@@ -74,6 +82,27 @@ func main() {
 		println(nn, n)
 		panic("numchars")
 	}
+
+	// (32-126; 95 glyphs total), an extra glyph representing an ellipsis
+	// - 1 unsigned byte: the maximum width of each glyph in the font, in pixels.
+	// - 1 unsigned byte: the height of glyphs, in pixels, including all vertical padding.
+	// - 1 unsigned byte: the number of horizontal pixels to advance between characters.
+	// - 96 glyph records
+	{
+		printd("%%%%FNT0")
+		var d []byte
+		d = append(d, byte(w), byte(h), byte(w))
+		d = append(d, []byte(dekglyph(make([]bool, w*h), w, h))...) //space
+		for i := 0; i < 94; i++ {
+			if len(dek[33+rune(i)]) == 0 {
+				panic("missing " + strconv.Itoa(i))
+			}
+			d = append(d, []byte(dek[32+rune(i)])...)
+		}
+		d = append(d, []byte(dek['.'])...) //todo "…"
+		printd(base64.StdEncoding.EncodeToString(d))
+		printd("\n")
+	}
 }
 
 func glyph(m image.Image, x, y, w, h int) string {
@@ -81,6 +110,7 @@ func glyph(m image.Image, x, y, w, h int) string {
 	for j := 0; j < h; j++ {
 		for i := 0; i < w; i++ {
 			if rr, gg, bb, _ := m.At(x+i, y+j).RGBA(); rr == 0 && gg == 0 && bb == 0 {
+				fmt.Println("r/g/b", rr, gg, bb)
 				b[k] = true
 			}
 			k++
@@ -101,8 +131,29 @@ func glyph(m image.Image, x, y, w, h int) string {
 			}
 		}
 		return string(h)
+	} else if φ == "decker" {
+		return dekglyph(b, w, h)
 	}
 	panic("φ")
+}
+func dekglyph(b []bool, w, h int) string {
+	// - 1 unsigned byte giving the true width of the glyph: how many pixels to advance horizontally after drawing the glyph.
+	//   - (width/8)*height bytes of packed image data, in which each byte represents 8 horizontally adjacent pixels.
+	//   - Glyphs with a width that is not evenly divisible by 8 will be padded with 0 bits. Note the similarity to IMG0.
+
+	w8 := w / 8
+	if 8*w8 < w {
+		w8++
+	}
+	u := append([]byte{}, 0)
+	for i := 0; i < h; i++ {
+		a := make([]bool, 8*w8)
+		copy(a, b[i*w:w+i*w])
+		for j := 0; j < 8*w8; j += 8 {
+			u = append(u, char(a[j:j+8]))
+		}
+	}
+	return string(u)
 }
 func hxb(x byte) (byte, byte) { h := "0123456789abcdef"; return h[x>>4], h[x&0x0F] }
 func char(b []bool) (c byte) {
@@ -120,6 +171,11 @@ func printk(f string, a ...interface{}) {
 }
 func printp(f string, a ...interface{}) {
 	if φ == "psf" {
+		fmt.Printf(f, a...)
+	}
+}
+func printd(f string, a ...interface{}) {
+	if φ == "decker" {
 		fmt.Printf(f, a...)
 	}
 }
