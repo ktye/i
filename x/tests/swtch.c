2a1ee775d3c0c5cf2641d75cf42b13dc7e99cd55
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2022-12-10 13:52:40 +0100

    cc switch

--- x/tests/swtch.c
+++ x/tests/swtch.c
@@ -1,20 +1,16 @@
 int32_t f(int32_t x){
  switch(x){
-  case 0:
-  return (1+x);
+  case 0:return (1+x);
   break;
-  default:
-  return x;
+  default:return x;
   break;
  };
 }
 int32_t g(int32_t x){
  switch(x){
-  case 0:
-  return (1+x);
+  case 0:return (1+x);
   break;
-  case 1:
-  return x;
+  case 1:return x;
   break;
  };
  return 0;
--- x/tests/swtch2.c
+++ x/tests/swtch2.c
@@ -4,11 +4,9 @@ int32_t f(int32_t x){
 int32_t g(int32_t x){
  int32_t r;
  switch(x){
-  case 0:
-  r=f(x);
+  case 0:r=f(x);
   break;
-  default:
-  {
+  default:{
    r=(x>5)?((x-3)):((x-2));
    r=r;
   }
