2c857d26a6e2e855ee8e6ff1b9f9f7760005cab2
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2022-12-12 10:56:05 +0100

    cc

--- x/tests/const.c
+++ x/tests/const.c
@@ -3,5 +3,5 @@ const double p=3.141592653589793;
 const int32_t I=-4;
 const uint32_t U=4;
 const uint32_t u2=0xfffffffe;
-const int64_t J=-5;
-const uint64_t K=0xfedcba0123456789;
+const int64_t J=-5ll;
+const uint64_t K=0xfedcba0123456789ull;
--- x/tests/lit.c
+++ x/tests/lit.c
@@ -3,5 +3,5 @@ double p = 3.141592653589793;
 int32_t I = -4;
 uint32_t U = 4;
 uint32_t u2 = 0xfffffffe;
-int64_t J = -5;
-uint64_t K = 0xfedcba0123456789;
+int64_t J = -5ll;
+uint64_t K = 0xfedcba0123456789ull;
