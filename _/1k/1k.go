cee262e043e330fb6bcef673c4cb78ff53dfb623
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2022-12-14 15:49:08 +0100

    1k

--- _/1k/1k.go
+++ _/1k/1k.go
@@ -8,30 +8,92 @@ var tot, top int32
 
 func init() {
 	Memory(1)
-	// no @ $ /: \: ':
+	// no @ $ /: \: ': ` [ ]  { }  may be assigned to as user functions but not infix
 
+	// x(space)y          is (,x),y
+	// e.g.:   l:4 5 6    is (4;5;6)
+	//         1 l 2      is (1;4 5 6;2)  or as:  1 (4 5 6) 2      (space matters)
+	//
+	// "abc" is quotation/literal, also used for lambdas: f:"(+/x)%#x"
 	//
 	// juxtaposition
-	// aa cal
-	// av cal
-	// va idx
-	// vv idv
-	Data(0, "\x3a\x2b\x2d\x2a\x25\x26\x7c\x3c\x3e\x3d\x7e\x21\x2c\x5e\x23\x5f\x3f\x2e\x27\x2f\x5c")
+	// ay cal
+	// vy idx
+	Data(0, "\x2b\x2d\x2a\x25\x26\x7c\x3c\x3e\x3d\x21\x3a\x7e\x2c\x5e\x23\x5f\x3f\x2e\x20\x27\x2f\x5c") // +-*%&|<>=!:~,^#_?. '/\  (22)
 
-	//      a     ???  abs  neg  fst  ???  zer  ???  ???  ???  ???  not  til  enl  ???  cnt  ???  ???  lup  ---  ---  ---
-	//      v     ???  flp  ngv  fst  rot  wer  rev  grd  gdn  grp  ntv  odo  enl  srt  cnt  dp1  unq  exe  ---  ---  ---
-	//              :    +    -    *    %    &    |    <    >    =    ~   !    ,     ^    #    _    ?    .    '    /    \`
-	//             58   43   45   42   37   38  124   60   62   61  126  33   44    94   35   95   63   46   39   47   92
-	//              0    1    2    3    4    5    6    7    8    9   10  11   12    13   14   15   16   17   18   19   20
-	Functions(00, asn, mtc, ny2, cat, pow, rep, ny2, del, cal, ech, ovr, scn) //aa
-	Functions(12, asn, nul, ny2, cat, cut, tak, drp, sfl, cal, ech, ovr, scn) //av
-	Functions(24, asn, nul, ny2, cat, pov, tka, dra, fnd, ati, ecv, ovv, scn) //va
-	Functions(36, asn, mtv, ny2, cat, ctv, tkv, drv, fnx, atv, ecv, ovv, scn) //vv
-	Functions(48, add, sub, mul, div, min, max, les, mor, eql, mod)           //scalar
-
+	//              :    ~    ,    ^    #    _    ?    .         '    /    \
+	Functions(00, asn, mtc, cat, cut, tak, drp, rol, cal, spc, ech, ovr, scn)                                    //ay
+	Functions(12, asn, mtc, cat, ctv, tkv, dpv, fnd, atx, spc, inn, spl, jon)                                    //vy
+	Functions(24, add, sub, mul, div, min, max, les, mor, eql, mod)                                              //scalar dyadic
+	Functions(34, flp, neg, fst, rot, wer, rev, grd, gdn, grp, til, idn, not, enl, str, cnt, lst, unq, val, enl) //monadic
+	//              +    -    *    %    &    |    <    >    =    !    :    ~    ,    ^    #    _    ?    .  spc    '   /  \
+	//             43   45   42   37   38  124   60   62   61   33   58  126   44   94   35   95   63   46   32   39  47  92
 }
 func main() {
+	k1()
+}
+func k1() {
 	tot = 65536
+	top = 24
+	rm(256) //keys at 24
+	rm(256) //vals at 270
+}
+
+func v(x int32) int32 { return x >> 1 }
+func w(x int32) int32 { return x << 1 }
+func n(x int32) int32 {
+	if x&1 != 0 {
+		return -1
+	}
+	return I32(v(x) - 4)
+}
+func mk(x int32) int32 {
+	x = x*4 + 4
+	for tot < top+x {
+		Memorygrow(1)
+		tot += 65536
+	}
+	r := top
+	SetI32(r, x)
+	top += x
+	return 1 + w(r)
+}
+func rm(x int32) int32 { // reset make, use with c1
+	r := mk(x)
+	SetI32(v(r)-1, 0)
+	return r
+}
+func c1(x, y int32) int32 {
+	p := I32(v(x))
+	SetI32(p, 1+p)
+	SetI32(v(x)+4*p, y)
+	return x
+}
+func l2(x, y int32) int32 { return cat(enl(x), enl(y)) }
+func el(x int32) int32 {
+	if n(x)&1 != 0 {
+		return x
+	}
+	return enl(x)
+}
+func ec2(f, x, y int32) int32 {
+	rn := max(cnt(x), cnt(y)) >> 1
+	r := mk(rn)
+	p := v(r)
+	for i := int32(0); i < rn; i++ {
+		SetI32(p+4*i, cal(f, cat(enl(atx(x, i)), enl(atx(y, i)))))
+	}
+	return r
+}
+func seq(x, o, m int32) int32 {
+	r := rm(x)
+	for i := int32(0); i < x; i++ {
+		c1(r, (i+o)%m)
+	}
+	return r
+}
+func lup(x int32) int32 {
+	return x //nyi
 }
 
 type f1 = func(int32) int32
@@ -84,52 +146,29 @@ func cat(x, y int32) int32 { // x,y
 	Memorycopy(v(r)+4*xn, v(y), 4*yn)
 	return r
 }
-func pow(x, y int32) int32 { // a^a
-	x, y = v(x), v(y)
-	r := int32(1)
-	for {
-		if y&1 == 1 {
-			r *= x
-		}
-		y >>= 1
-		if y == 0 {
-			break
-		}
-		x *= x
-	}
-	return w(r)
-}
-func pov(x, y int32) int32 { return ec2(94, x, y) }               // v^a
-func cut(x, y int32) int32 { return ny2(x, y) }                   // a^v
-func ctv(x, y int32) int32 { return ny2(x, y) }                   // v^v
-func rep(x, y int32) int32 { return atv(y, til(x)) }              // a#a
-func tak(x, y int32) int32 { return atv(y, max(cnt(y), til(x))) } // a#v
-func tka(x, y int32) int32 { return tka(x, enl(y)) }              // v#a (overload?)
-func tkv(x, y int32) int32 { return atv(y, wer(inn(y, x))) }      // v#v
-func drt(x, y int32) int32 { return drp(x, til(y)) }              // a_a   x_!y
-func drp(x, y int32) int32 { // a_v
+func cut(x, y int32) int32 { return ny2(x, el(y)) }                           // a^y
+func ctv(x, y int32) int32 { return ny2(x, el(y)) }                           // v^y
+func tak(x, y int32) int32 { return atx(el(y), cal(66, l2(til(x), cnt(x)))) } // a#v
+func tkv(x, y int32) int32 { return atx(y, wer(inn(el(y), x))) }              // v#y
+func drp(x, y int32) int32 { // a_y
 	yn := n(y)
 	xv := v(x)
 	if x < 0 {
 		if xv < -yn {
-			return cat(zer(w(yn+xv)), x)
-		} else {
-			return tak(x, w(-xv))
+			return atx(x, seq(-xv, xv+yn, yn))
 		}
+		return tak(x, w(-xv))
 	} else {
 		if xv > y {
 			return mk(0)
-		} else {
-			return add(x, til(w(yn-xv)))
 		}
+		return atx(x, seq(yn-xv, xv, yn))
 	}
 }
-func dra(x, y int32) int32 { return drv(x, til(cnt(x))) }         // v_a
-func drv(x, y int32) int32 { return atv(y, wer(not(inn(y, x)))) } // v_v
-func del(x, y int32) int32 { return ny2(x, y) }                   // a?a
-func sfl(x, y int32) int32 { return ny2(x, y) }                   // a?v
-func fnd(x, y int32) int32 { return fst(ec2(126, x, y)) }         // v?a
-func fnx(x, y int32) int32 { return ec2(63, enl(x), y) }          // v?v   x?/:y
+func dpv(x, y int32) int32 { return atx(y, wer(not(inn(el(y), x)))) } // v_y
+func rol(x, y int32) int32 { return ny2(x, y) }                       // a?y
+func fnd(x, y int32) int32 { return fst(ec2(126, x, y)) }             // v?a
+func fnx(x, y int32) int32 { return ec2(63, enl(x), y) }              // v?v   x?/:y
 func cal(x, y int32) int32 { // a.a  a.v
 	y = el(y)
 	yn := n(y)
@@ -140,7 +179,7 @@ func cal(x, y int32) int32 { // a.a  a.v
 		}
 	}
 	if i > 21 {
-		return prs(lup(x), y)
+		return exe(lup(x), y)
 	}
 	x = fst(y)
 	y = I32(4 + v(y))
@@ -160,6 +199,65 @@ func cal(x, y int32) int32 { // a.a  a.v
 	}
 	return Func[i].(f2)(x, y)
 }
+func atx(x, y int32) int32 { // v.a  (also a.v)
+	nn := n(y)
+	if nn < 0 {
+		return ec2(46, enl(x), y)
+	}
+	nn = n(x)
+	if nn < 0 {
+		return x
+	}
+	y = v(y)
+	if y < 0 || nn > y {
+		return 0
+	} else {
+		return I32(v(x) + 4*y)
+	}
+}
+func spc(x, y int32) int32 { return cat(enl(x), y) } // x(space)y
+func ech(x, y int32) int32 { // a'a  a'v
+	yn := v(cnt(y))
+	r := mk(yn)
+	p := v(r)
+	for i := int32(0); i < yn; i++ {
+		SetI32(p, cal(x, atx(y, w(i))))
+		p += 4
+	}
+	return r
+}
+func inn(x, y int32) int32 {
+	yn := w(n(y))
+	if n(x) < 0 {
+		return mor(yn, fnx(y, x))
+	} else {
+		return ovr(124, mor(yn, fnd(y, x))) // |/(#y)>y?x
+	}
+}
+func ovr(x, y int32) int32 { // a/a  a/v
+	yn := v(cnt(y))
+	r := fst(x)
+	for i := int32(1); i < yn; i++ {
+		r = cal(x, atx(y, w(i)))
+	}
+	return r
+}
+func spl(x, y int32) int32 { return cut(cat(0, wer(ec2(126, enl(x), fst(y)))), x) } // v/a    (0,&(,x)~'*y)^x
+func scn(x, y int32) int32 { // a\a  a\v
+	yn := v(cnt(y))
+	r := mk(yn)
+	p := v(r)
+	f := fst(x)
+	SetI32(p, f)
+	for i := int32(0); i < yn; i++ {
+		p += 4
+		f = cal(x, cat(enl(f), enl(atx(y, w(i)))))
+		SetI32(p, f)
+	}
+	return r
+}
+func jon(x, y int32) int32 { return ovr(44, ec2(44, x, enl(y))) } // v\a   ,/x,',y
+
 func nm(f, x, a int32) int32 {
 	if a != 0 {
 		return ech(46, cat(f, enl(x)))
@@ -172,66 +270,78 @@ func nd(f, x, y, a int32) int32 {
 	}
 	return w(Func[f].(f2)(v(x), v(y)))
 }
-func ati(x, y int32) int32 { // v.a  (also a.v)
-	xn := n(x)
-	if xn < 0 {
-		return x
-	}
-	yv := v(y)
-	if yv < 0 || xn > yv {
-		return 0
-	} else {
-		return I32(v(x) + 4*yv)
-	}
-}
-func atv(x, y int32) int32 { return ec2(46, enl(x), y) } // v.v  x./:y
-func ech(x, y int32) int32 { // a'a  a'v
-	yn := v(cnt(y))
-	r := mk(yn)
-	p := v(r)
-	for i := int32(0); i < yn; i++ {
-		SetI32(p, cal(x, ati(y, w(i))))
-		p += 4
-	}
-	return r
-}
+
 func ecv(x, y int32) int32 { return ec2(39, x, enl(y)) } // v'a  v'a
-func ovr(x, y int32) int32 { // a/a  a/v
-	yn := v(cnt(y))
-	r := fst(x)
-	for i := int32(1); i < yn; i++ {
-		r = cal(x, ati(y, w(i)))
-	}
-	return r
-}
+
 func ovv(x, y int32) int32 { return ec2(47, x, enl(y)) } // v/a  v/v
-func scn(x, y int32) int32 { // a\a  a\v
-	yn := v(cnt(y))
-	r := mk(yn)
-	p := v(r)
-	f := fst(x)
-	SetI32(p, f)
-	for i := int32(0); i < yn; i++ {
-		p += 4
-		f = cal(x, cat(enl(f), enl(ati(y, w(i)))))
-		SetI32(p, f)
-	}
-	return r
-}
 
 //monadic
 func nyi(x int32) int32 { return x }
-func enl(x int32) int32 { r := mk(1); SetI32(r, x); return r }
-func til(x int32) int32 {
-	x = v(x)
-	r := mk(x)
-	p := v(r)
-	for i := int32(0); i < x; i++ {
-		SetI32(p+4*i, w(i))
+func flp(x int32) int32 { return nyi(x) } // +x
+func neg(x int32) int32 { // -x
+	if n(x) < 0 {
+		x = v(x)
+		if x < 0 {
+			x = -x
+		}
+		return w(x)
+	}
+	return ech(90, x)
+}
+func fst(x int32) int32 { return atx(x, 0) }              // *x
+func rot(x int32) int32 { return cat(drp(2, x), fst(x)) } // %x
+func wer(x int32) int32 { // &x
+	x = el(x)
+	r := mk(0)
+	xn := n(x)
+	for i := int32(0); i < xn; i++ {
+		j := w(i)
+		r = cat(r, tak(j, atx(x, j)))
 	}
 	return r
 }
-func cnt(x int32) int32 {
+func rev(x int32) int32 { // |x
+	x = el(x)
+	xn := n(x)
+	r := rm(xn)
+	for i := int32(0); i < xn; i++ {
+		c1(r, xn-i-1)
+	}
+	return atx(x, r)
+}
+func grd(x int32) int32 { return nyi(el(x)) }               // <x
+func gdn(x int32) int32 { return nyi(el(x)) }               // >x
+func grp(x int32) int32 { return nyi(el(x)) }               // =x
+func til(x int32) int32 { return seq(v(x), 0, 2147483647) } // !0
+func idn(x int32) int32 { return x }                        // :x
+func not(x int32) int32 { // ~a
+	if n(x) < 0 {
+		return w(I32B(v(x) != 0))
+	}
+	return ech(252, x)
+}
+func enl(x int32) int32 { r := mk(1); SetI32(r, x); return r } // ,x
+func str(x int32) int32 { // ^x
+	if n(x) < 0 {
+		x = v(x)
+		if x < 0 {
+			return cat(90, str(w(-x)))
+		} else if x == 0 {
+			return enl(0)
+		}
+		r := rm(10)
+		i := int32(0)
+		for x > 0 {
+			c1(r, x%10)
+			x -= 10
+			x /= 10
+			i++
+		}
+		return tak(w(i), r)
+	}
+	return ech(188, x)
+}
+func cnt(x int32) int32 { // #x
 	xn := n(x)
 	if xn < 0 {
 		return 2
@@ -239,69 +349,67 @@ func cnt(x int32) int32 {
 		return w(xn)
 	}
 }
-func zer(x int32) int32 { return mk(v(x)) } // &a
-func wer(x int32) int32 { // &v
-	r := mk(0)
+func lst(x int32) int32 { return atx(el(x), w(n(x)-1)) } // _x
+func unq(x int32) int32 { return nyi(x) }                // ?x
+func val(x int32) int32 { // .x
 	xn := n(x)
-	for i := int32(0); i < xn; i++ {
-		j := w(i)
-		r = cat(r, rep(j, ati(x, j)))
+	if xn < 0 {
+		return lup(x)
+	}
+	return exe(x, 0)
+}
+func tok(r, x, t int32) int32 {
+	xn := n(x)
+	r := rm(xn)
+	x = v(x)
+	xe := x + 4*xn
+	t := 0
+	q := rm(xn)
+	for x < xe {
+		c := I32(x)
+		if 0 < n(q) {
+			if c == 68 { // "
+				c1(r, q)
+				q = rm(xn)
+				continue
+			}
+			c1(q, c)
+			continue
+		}
+		if c >= 96 && c <= 114 {
+			t *= 10
+			t += v(c - 36)
+			continue
+		}
+		c1(r, x)
+		x += 4
 	}
 	return r
 }
-func not(x int32) int32 { return w(I32B(v(1) == 0)) } // ~a
-func ntv(x int32) int32 { return ech(252, x) }        // ~v
-func fst(x int32) int32 { return ati(x, 0) }          // *a  *v
-
-func ec2(f, x, y int32) int32 {
-	rn := max(cnt(x), cnt(y)) >> 1
-	r := mk(rn)
-	p := v(r)
-	for i := int32(0); i < rn; i++ {
-		SetI32(p+4*i, cal(f, cat(enl(ati(x, i)), enl(ati(y, i)))))
-	}
-	return r
+func prs(r, x, t int32) int32 {
+	// 11-(1+1)+-1
+	//          -   1)     >   A       ? /    >d
+	//          +  -1)     m   B       - -    m>
+	//          )  +1)     >   C       1 -    >d
+	//          1  )+1)    >   D       1 1    c
+	//                                 ( ?    <>
+	//          +  1)+1)   >   A     else     >
+	//          1  +1)+1)  d   E
+	//          (  1)+1)   b<  F      -/1()
+	//          1    +1)   d   E
+	//          -    1)    >   A
+	//          1    -1)   d   E
+	//          1    1)    j   G
+	//               1)
+	//          +    +)    m
+	//
+	// x == (      => move<y over (, remove )  next
+	// y == /      => move> dyadic
+	// y == -
+	//   | x == 1  => move> dyadic
+	//   | x == -  => monadic, move>
+	// else        => move
 }
-
-func inn(x, y int32) int32 {
-	yn := w(n(y))
-	if n(x) < 0 {
-		return mor(yn, fnx(y, x))
-	} else {
-		return ovr(124, mor(yn, fnd(y, x))) // |/(#y)>y?x
-	}
-}
-
-func el(x int32) int32 {
-	if n(x)&1 != 0 {
-		return x
-	}
-	return enl(x)
-}
-
-func v(x int32) int32 { return x >> 1 }
-func w(x int32) int32 { return x << 1 }
-func n(x int32) int32 {
-	if x&1 != 0 {
-		return -1
-	}
-	return I32(v(x) - 4)
-}
-
-func mk(x int32) int32 {
-	x = x*4 + 4
-	for tot < top+x {
-		Memorygrow(1)
-		tot += 65536
-	}
-	r := top
-	SetI32(r, x)
-	top += x
-	return 1 + w(r)
-}
-func lup(x int32) int32 {
-	return x //nyi
-}
-func prs(x, y int32) int32 {
-	return x //nyi
+func exe(x, a int32) int32 {
+	x = rev(run(tok(x)), 0)
 }
