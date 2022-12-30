593bf7303f10a612b8cedf7c10022e9268c6200c
elmar <ktye@users.noreply.github.com>
2022-12-30 23:30:02 +0100

    js

--- x/tests/const.js
+++ x/tests/const.js
@@ -3,5 +3,5 @@ const p=F(3.141592653589793);
 const I=I(-4);
 const U=U(4);
 const u2=U(0xfffffffe);
-const J=J(-5);
+const J=J(-5n);
 const K=K(BigInt("0xfedcba0123456789"));
--- x/tests/lit.js
+++ x/tests/lit.js
@@ -3,5 +3,5 @@ let p = F(3.141592653589793);
 let I = I(-4);
 let U = U(4);
 let u2 = U(0xfffffffe);
-let J = J(-5);
+let J = J(-5n);
 let K = K(BigInt("0xfedcba0123456789"));
