418f127a5729349a810e2bc10c34ebed9d133e9a
elmar <ktye@users.noreply.github.com>
2022-12-31 18:19:13 +0100

    js

--- x/tests/const.js
+++ x/tests/const.js
@@ -1,7 +1,7 @@
 const F=F(1.23);
 const p=F(3.141592653589793);
 const I=I(-4);
-const U=U(4);
-const u2=U(0xfffffffe);
+const U=U(4n);
+const u2=U(-2n);
 const J=J(-5n);
 const K=K(BigInt("0xfedcba0123456789"));
