7710c003d14a17e4f06129819150dab94a9e6221
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2023-01-31 14:26:23 +0100

    ku.c ku.in.k log

--- ku/ku.c
+++ ku/ku.c
@@ -7,11 +7,13 @@ uint64_t cnative(uint64_t x,uint64_t y);
 #undef max
 #include"k.h"
 
-int32_t W,H,SC;
-uint32_t *O;
+int32_t W,H,SC;     //dimensions,scale(1|2)
+uint32_t *O;        //pixels
+uint32_t *kui=NULL; //event log
+int nui=0,aui=0;
 
 // todo conform xI yI
-uint64_t cnative(uint64_t x,uint64_t y){ //P[color;indexlist]
+uint64_t cnative(uint64_t x,uint64_t y){ //P[color;pixelindex] /ci;pI
  x=Fst(rx(y));y=Las(y);
  dx(x);dx(y);if((tp(x)!=it)||(tp(y)!=It))return 0;
  int32_t  n=nn(y),yp=(int32_t)y>>2;
@@ -48,19 +50,22 @@ int shift(int k,int s){
  return k;
 }
 
-int main(int args, char **argv){
- if((args>1)&&(argv[1][0]=='2')){args--;argv++;SC=2;}else SC=1;
- args_=args;
- argv_=argv;
- init();
- kinit();
- Asn(sc(Ku('P')),((l2(0,Ki(1)))&0xffffffff)|((uint64_t)xf<<59)); //P:native func
- dx(val(ks(p0)));
- 
- doargs();
- W=(int32_t)Val(sc(Ku('W')));
- H=(int32_t)Val(sc(Ku('H')));
- printf("W=%d H=%d %d\n", W, H, SC*W*H);
+void logspc(void)   {if(nui==aui)kui       =(uint32_t*)realloc(kui,aui=4*nui*2);}
+int logkey(int x)      {logspc();kui[nui++]=((uint32_t)x)&0x0000ffff;return x;}
+int logmas(int x,int y){logspc();kui[nui++]=((x&0xffff)<<16)|(y&0xffff)|0x80000000;}
+int writekui(){
+ FILE *f=fopen("ku.in.k", "w");
+ fprintf(f,"P:{y}\n");
+ for(int i=0;i<nui;i++){uint32_t x=kui[i];
+  if(x&0x80000000)fprintf(f,"M[%u,%u]\n",(x&0x7fff0000)>>16,x&0xffff);
+  else            fprintf(f,"K %u\n",x);
+ }
+ fclose(f);
+ printf("w ku.in.k\n");
+ return 1;
+}
+
+int fensterl(){ //verb: fens·terln
  uint64_t M=sc(Ku('M'));
  uint64_t K=sc(Ku('K'));
  O=calloc(SC*SC*W*H,4);
@@ -73,12 +78,28 @@ int main(int args, char **argv){
  fenster_open(&f);
  int64_t now=fenster_time();
  while(!fenster_loop(&f)){
-  for(int i=0;i<128;i++)if(f.keys[i]){dx(Atx(Val(K),Ki(shift(i,f.mod&2))));f.keys[i]=0;}
+  for(int i=0;i<128;i++)if(f.keys[i]){dx(Atx(Val(K),Ki(logkey(shift(i,f.mod&2)))));f.keys[i]=0;}
   if(f.keys[27])break;
-  if(f.mouse){dx(Atx(Val(M),Cat(Ki((int32_t)f.x/SC),Ki((int32_t)f.y/SC))));f.mouse=0;}
+  if(f.mouse){f.x/=SC;f.y/=SC;logmas(f.x,f.y);dx(Atx(Val(M),Cat(Ki((int32_t)f.x),Ki((int32_t)f.y))));f.mouse=0;}
   int64_t time=fenster_time();
   if(time-now<1000/60)fenster_sleep(time-now);
   now=time;
  }
  fenster_close(&f);
+ return 0;
 }
+int main(int args, char **argv){
+ if((args>1)&&(argv[1][0]=='2')){args--;argv++;SC=2;}else SC=1;
+ args_=args;
+ argv_=argv;
+ init();
+ kinit();
+ Asn(sc(Ku('P')),((l2(0,Ki(1)))&0xffffffff)|((uint64_t)xf<<59)); //P:native func
+ dx(val(ks(p0)));
+ doargs();
+ W=(int32_t)Val(sc(Ku('W')));
+ H=(int32_t)Val(sc(Ku('H')));
+ printf("W=%d H=%d %d\n", W, H, SC*W*H);
+ return(!setjmp(_jb_))?fensterl():writekui();
+}
+
