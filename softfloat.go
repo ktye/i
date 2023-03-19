87a8394c01d25182da97dd7bfec31614feb24e0a
elmar <ktye@users.noreply.github.com>
2023-03-19 13:53:31 +0100

    sf

--- softfloat.go
+++ softfloat.go
@@ -10,6 +10,21 @@ func fadd(x, y float64) float64 { return 0 }
 func fsub(x, y float64) float64 { return 0 }
 func fmul(x, y float64) float64 { return 0 }
 func fdiv(x, y float64) float64 { return 0 }
+
+func fsgn(x uint64) int32 { return int32(x>>32)&(1<<32) }
+func fmnt(x uint64) uint64 { return x & 4503599627370495 }
+func fexp(x uint64) int32 { return int32(x>>52) & 2047 }
+func fnan(x uint64) int32 { return I32B(fexp(x) == 2047) }
 */
+
+/*
+mantbits64 uint = 52
+expbits64  uint = 11
+bias64          = -1<<(expbits64-1) + 1
+nan64 uint64 = (1<<expbits64-1)<<mantbits64 + 1<<(mantbits64-1) // quiet NaN, 0 payload
+inf64 uint64 = (1<<expbits64 - 1) << mantbits64
+neg64 uint64 = 1 << (expbits64 + mantbits64)
+*/
+
 
 // https://go.dev/src/runtime/softfloat64.go
