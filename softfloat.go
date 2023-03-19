996592a1a89bf49ab73e0266c2347da6c46bde66
elmar <ktye@users.noreply.github.com>
2023-03-19 11:31:16 +0100

    sf

---
+++ softfloat.go
@@ -0,0 +1,15 @@
+// +build:ignore
+package main
+
+// why? be kos
+
+/*
+func fneg(x float64) float64 { return 0 }
+func fsqt(x float64) float64 { return 0 }
+func fadd(x, y float64) float64 { return 0 }
+func fsub(x, y float64) float64 { return 0 }
+func fmul(x, y float64) float64 { return 0 }
+func fdiv(x, y float64) float64 { return 0 }
+*/
+
+// https://go.dev/src/runtime/softfloat64.go
