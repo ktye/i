ac54d0afe9aeae217051e94e669b00aead16307d
elmar <ktye@users.noreply.github.com>
2024-01-04 18:01:33 +0100

    types

--- _/avx/main.c
+++ _/avx/main.c
@@ -15,6 +15,9 @@ compressqi      mask_compress_epi8(y,z,x)
 #define V4 At(vector_size(1<<4),aligned(1))
 #define V6 At(vector_size(1<<6),aligned(1))
 typedef unsigned char Z V6;
+typedef unsigned i2;
+typedef unsigned long u;
+typedef float e2;
 
 #define bu(f) __builtin_##f
 #define bo(f) bu(ia32_##f)
@@ -27,42 +30,42 @@ typedef unsigned char Z V6;
 #define zo(f) _ia32_##f##512
 #define zm(f) _ia32_##f##512_mask
 
-Z _ia32_sqrtps512(float V6 x, int y){float V6 r;for(int i=0;i<16;i++)r[i]=__builtin_sqrtf(x[i]);return r;}
-Z _ia32_permvarqi512(Z x, Z y){Z r;for(int i=0;i<64;i++)r[i]=y[x[i]];return r;}
-Z _ia32_permvarsi512(Z x, Z y){unsigned int V6 a=x,b=y,r; for(int i=0;i<16;i++)r[i]=b[a[i]]; return r;}
-Z _ia32_vpermi2varps512(Z x, Z y, Z z){unsigned int V6 a=x,i=y,b=z,r;int m;for(int j=0;j<16;j++){m=i[j]&0x0f;r[j]=((0xf0&i[j]))?b[m]:a[m];};return r; }
-Z _ia32_vpermi2varqi512(Z x, Z y, Z z){Z r;int m;for(int i=0;i<64;i++){m=63&y[i];r[i]=(64&y[i])?z[m]:x[m];};return r;}
-Z _ia32_selectb_512(unsigned long k, Z a, Z w){Z r;for(int i=0;i<64;i++)r[i]=(1&(k>>i))?a[i]:w[i]; return r;}
-unsigned long _ia32_cvtb2mask512(Z x){unsigned long r=0;for(int i=0;i<64;i++)r|=(x[i]>>7)?(1<<i):0; return r;}
-Z _ia32_compressqi512_mask(Z a, Z s, unsigned long k){Z r=s;int m=0;for(int i=0;i<64;i++){if(1&k>>i){r[m++]=a[i];}};return r;}
+Z _ia32_sqrtps512(e2 V6 x, int y){e2 V6 r;for(u i=0;i<16;i++)r[i]=__builtin_sqrtf(x[i]);return r;}
+Z _ia32_permvarqi512(Z x, Z y){Z r;for(u i=0;i<64;i++)r[i]=y[x[i]];return r;}
+Z _ia32_permvarsi512(Z x, Z y){i2 V6 a=x,b=y,r; for(u i=0;i<16;i++)r[i]=b[a[i]]; return r;}
+Z _ia32_vpermi2varps512(Z x, Z y, Z z){i2 V6 a=x,i=y,b=z,r;int m;for(int j=0;j<16;j++){m=i[j]&0x0f;r[j]=((0xf0&i[j]))?b[m]:a[m];};return r; }
+Z _ia32_vpermi2varqi512(Z x, Z y, Z z){Z r;int m;for(u i=0;i<64;i++){m=63&y[i];r[i]=(64&y[i])?z[m]:x[m];};return r;}
+Z _ia32_selectb_512(u k, Z a, Z w){Z r;for(u i=0;i<64;i++)r[i]=(1&(k>>i))?a[i]:w[i]; return r;}
+u _ia32_cvtb2mask512(Z x){u r=0;for(u i=0;i<64;i++)r|=(x[i]>>7)?(1<<i):0; return r;}
+Z _ia32_compressqi512_mask(Z a, Z s, u k){Z r=s;int m=0;for(u i=0;i<64;i++){if(1&k>>i){r[m++]=a[i];}};return r;}
 
 #endif
 
 #include<stdio.h>
 
-void P(Z x){for(int i=0;i<64;i++)printf("%02x ",x[i]);printf("\n");}
-void Q(unsigned int V6 x){for(int i=0;i<16;i++)printf("%08x ",x[i]);printf("\n");}
-void F(float V6 x){for(int i=0;i<16;i++)printf("%6.3f ",x[i]);printf("\n");}
-Z iota(){Z r;for(int i=0;i<64;i++)r[i]=i;return r;}
-Z jota(){unsigned int V6 r;for(int i=0;i<16;i++)r[i]=i;return r;}
-Z reva(){Z r;for(int i=0;i<64;i++)r[i]=63-i;return r;}
-Z rewa(){unsigned int V6 r;for(int i=0;i<16;i++)r[i]=15-i;return r;}
-Z alta(){unsigned int V6 r;for(int i=0;i<16;i++)r[i]=(i%2)?0:-1;return r;}
+void P(Z x){for(u i=0;i<64;i++)printf("%02x ",x[i]);printf("\n");}
+void Q(i2 V6 x){for(u i=0;i<16;i++)printf("%08x ",x[i]);printf("\n");}
+void F(e2 V6 x){for(u i=0;i<16;i++)printf("%6.3f ",x[i]);printf("\n");}
+Z iota(){Z r;for(u i=0;i<64;i++)r[i]=i;return r;}
+Z jota(){i2 V6 r;for(u i=0;i<16;i++)r[i]=i;return r;}
+Z reva(){Z r;for(u i=0;i<64;i++)r[i]=63-i;return r;}
+Z rewa(){i2 V6 r;for(u i=0;i<16;i++)r[i]=15-i;return r;}
+Z alta(){i2 V6 r;for(u i=0;i<16;i++)r[i]=(i%2)?0:-1;return r;}
 Z sqrtps(){
- float V6 x;for(int i=0;i<16;i++)x[i]=(float)i;
+ e2 V6 x;for(u i=0;i<16;i++)x[i]=(e2)i;
  return zo(sqrtps)(x,4);}
 Z permvarqi(){
  Z x=iota(),y=iota();
  y[1]=4;y[2]=3;
  return zo(permvarqi)(x,y);}
 Z permvarsi(){
- unsigned int V6 x=jota(),y=jota();
+ i2 V6 x=jota(),y=jota();
  y[1]=4;y[2]=3;
  return zo(permvarsi)(x,y);}
 
 Z vpermi2varps(){
- float V6 x=jota(),z=rewa();
- unsigned int V6 y=jota();
+ e2 V6 x=jota(),z=rewa();
+ i2 V6 y=jota();
  y[0]=5;y[1]=2;y[2]=9;y[3]=8;y[4]=1;
  y[2]|=0xf0;
  y[4]|=0x10;
@@ -74,16 +77,16 @@ Z vpermi2varqi(){
  y[4]|=128;
  return zo(vpermi2varqi)(x,y,z);}
 Z selectb_(){
- unsigned long k=123456789;
+ u k=123456789;
  Z x=iota(),y=reva();
  return zo(selectb_)(k,x,y);}
-unsigned long cvtb2mask(){
+u cvtb2mask(){
  Z x=iota();
  x[2]|=128;
  x[4]|=64;
  return zo(cvtb2mask)(x);}
 Z compressqi(){
- unsigned long k=0xff0fff0000000000;
+ u k=0xff0fff0000000000;
  Z x=iota(),y=reva();
  return zm(compressqi)(x,y,k);
 }
