733885d91c8d5c2ae66be544c699281561db55a9
elmar <ktye@users.noreply.github.com>
2025-08-10 01:32:41 +0200

    curl

--- k+/l.c
+++ k+/l.c
@@ -2,6 +2,9 @@ enum fn {
 #ifdef BLAS
   nrm2, asum, imax, rot, mv,
 #endif
+#ifdef CURL
+  curl,
+#endif
 };
 void reg(int64_t id, int64_t name, int32_t arity){
  int64_t l=ti(14,(uint32_t)l2(id, Ku(name)));
@@ -14,7 +17,10 @@ void libs(void){ //encode strings with: https://play.golang.org/p/4ethx6OEVCR
  reg(asum,1836413793ull,1);
  reg(imax,2019650921ull,1);
  reg(rot,    7630706ull,4);
- reg(mv,      30317ull, 7);
+ reg(mv,       30317ull,7);
+#endif
+#ifdef CURL
+ reg(curl,1819440483ull,1);
 #endif
 }
 
@@ -41,6 +47,16 @@ void cblas_dgemv(int,int,int,int,double,double*,int,double*,int,double,double*,i
 #define trans(x) (111+x)
 #endif
 
+#ifdef CURL
+void*curl_easy_init(void);
+int  curl_easy_perform(void*);
+void curl_easy_setopt(void*,int,void*);
+void curl_easy_cleanup(void*);
+static size_t curlback(void *c,size_t n,size_t m,uint64_t*r){
+ uint64_t x=*r;int32_t xn=nn(x);n*=m;x=uspc(x,Ct,n);*r=x;
+ memcpy(M_+(int32_t)x+xn,c,n);return n;}
+#endif
+
 double*FK(uint64_t x){return (double*)(M_+(int32_t)x);       }
 double f0(uint64_t x){return F64((int32_t)I64((int32_t)x  ));}
 double f1(uint64_t x){return F64((int32_t)I64((int32_t)x+8));}
@@ -48,6 +64,7 @@ double f4(uint64_t x){return F64((int32_t)I64((int32_t)x+32));}
 int32_t i2(uint64_t x){return I32((int32_t)I64((int32_t)x+16));}
 int32_t i3(uint64_t x){return I32((int32_t)I64((int32_t)x+24));}
 void dim(uint64_t A,int32_t rA,int32_t*m,int32_t*n){rA<0?(*m=-rA,*n=-rA/nn(A)):(*m=rA,*n=rA/nn(A));}
+
 
 //x:function-id(not refcounted), y:list of args(length arity)
 int64_t cnative(int64_t x, int64_t y){
@@ -78,6 +95,19 @@ int64_t cnative(int64_t x, int64_t y){
   }
  };
 #endif
+#ifdef CURL
+ case curl:{x=Fst(y);if(tp(x)!=Ct)trap();x=cat1(x,Kc(0));
+  void*c=curl_easy_init();
+  curl_easy_setopt(c,10002,M_+(int32_t)x);
+  uint64_t r=mk(Ct,0);
+  curl_easy_setopt(c,20011,curlback);
+  curl_easy_setopt(c,10001,(void*)&r);
+  curl_easy_setopt(c,10018,"libcurl-agent/1.0");
+  if(curl_easy_perform(c))trap();
+  curl_easy_cleanup(c);
+  dx(x);return r;}
+#endif
  default: return y;
 }}
+
 
