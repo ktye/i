06921b85cd684283c23357e3718e258dafd53e42
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2025-11-17 17:57:06 +0100

    l8: setjmp

--- x/l8.h
+++ x/l8.h
@@ -15,8 +15,6 @@ __attribute((naked))long sread( long fd,char*b,uint n){asm("xor %rax,%rax;syscal
 __attribute((naked))long swrite(long fd,char*b,uint n){asm("mov $1,%rax;syscall;ret");}
 __attribute((naked))void Exit(int x){asm("mov $60,%rax;syscall");}
 
-//uint csr=0xbfc0;      // ldmxcsr(&csr); (must be a memarg)
-//__attribute((naked))void ldmxcsr(uint*x){asm("ldmxcsr (%rdi);ret");}
                     static double F64copysign(double x,double y){union{double f;ulong i;}ux={x},uy={y};ux.i&=-1ul/2;ux.i|=uy.i&1ul<<63;return ux.f;}
                     static double F64floor(double x){long i=(long)(x<0?x-1.:x);return(long)i;}
 __attribute((naked))static double F64abs(double x){ asm("xor %eax,%eax;dec %rax;shr %rax;movq %rax,%xmm1;andpd %xmm1,%xmm0;ret;");}
@@ -31,23 +29,93 @@ static void wl(ulong x){ //debug-only
  swrite(1,s+i,32-i);}
 
 static char*M_;
+static ulong brk0;
 #define I_ ((int*)M_)
 #define U_ ((ulong*)M_)
 static void*F_[];   //dispatch table
 #define int32_t int //some literals
-static ulong pages_=0;
+static ulong pages_=0,pages__=0;  //a page is 64k
 static int Memorysize(void){return pages_;}
-static int Memorygrow(int d){pages_+=d;if(pages_>16384)return -1;brk((pages_<<16)+(ulong)M_);return pages_-d;}
-static void Memory(int x){M_=(char*)brk(0);Memorygrow(x);}
+static int Memorygrow(int d){pages_+=d;if(pages_>16384)return -1;brk(brk0+(pages_<<16));return pages_-d;}
+static void Memory(int x){brk0=brk(0);M_=(char*)brk0;Memorygrow(x);}
 static void Memory2(int x){}
 
-//todo try/catch
-static int jb_,jb__;
-static int setjmp(int x){return 0;}
-static int Memorycopy2(int x,int y,int z){return 0;}
-static int Memorycopy3(int x,int y,int z){return 0;}
-static int Memorysize2(void){return 2;}
-static int Memorygrow2(int x){return 2;}
+//try/catch
+static void*jb_[5];static int jb__=0;
+#define setjmp  __builtin_setjmp
+#define longjmp __builtin_setlongjmp
+static void panic(int x){if(!jb__)Exit(1);longjmp(jb_,1); }
+static int Memorysize2(void){return 0; }
+static void Memorygrow2(int x){}
+static int Memorycopy2(void/*int x,int y,int z*/){if(pages__){/*copy backwards*/ ulong o=(ulong)M_-brk0;for(int i=0;i<n;i++)U_[i-o]=U[i];M_=(char*)brk0; }
+ pages__=pages_;brk(brk0+(pages_<<17));ulong n=pages_<<13;for(int i=0;i<n;i++)U_[i+n]=U_[i];M_=brk0+(pages_<<16);return 0;}
+static int Memorycopy3(int x,int y,int z){pages_=pages__;pages__=0;M_=(char*)brk0;brk(brk0+(pages_<<16));return 0;}
+/*
+void main_(void){
+ ulong x2;
+ kinit();
+ doargs();
+ write(Ku(0x000a6b2f6579746bull));
+ store();
+ for(;;){
+  write(Ku(32ull));
+  x2=readfile(mk(Ct,0));
+  try(x2);
+ }
+}
+static void store(void){ //we ignore everything, just use the call to Memorycopy2
+ int g;
+ g=((1<<(I32(128)-16))-Memorysize2());
+ if(g>0){
+  Memorygrow2(g);
+ }
+ Memorycopy2(0,0,((int)(1)<<I32(128)));
+}
+static void catch(void){ //catch is Memorysize2, the size calculation is ignored
+ Memorycopy3(0,0,((int)(65536)*Memorysize2()));
+}
+static void try(ulong x){
+ jb__=1;if(!setjmp(jb_)){;
+  repl(x);
+  store();
+ }else{catch();}
+}
+*/
+
+/*
+ brk0<--pages_-->brk
+  M_...
+  +--------------+     without try/catch   M_ is always brk0, brk increases occasionally
+
+
+ brk0            M_             brk
+  +--------------+--------------+                  store() initial call: double memory, copy forward set new M_
+         +----------------^ copy forward left-to-right is ok
+
+ brk0   pages__            pages_           brk
+  +--------------+---------------------------+     during execution (try) active k increases memory (pages_>pages__)
+     old k mem           active k mem
+
+
+ brk0   pages__            
+  M_                      brk
+  +------------------------+-----------------+     store() (no error) copy backwards, reset M_, shrink brk
+          ^---------------------+  left-to-right
+
+ brk0      pages__         M_         pages_          brk   prepare for next try:
+  +------------------------+---------------------------+    increase brk, copy forward
+
+
+catch():
+ brk0     pages_          brk
+  M_                              in case of errors: reset
+  +------------------------+      Memorycopy3 does not copy anything but resets pages_, M_ and brk,  pages__=0
+  
+
+in k.c, store() calls Memorycopy2 (calls to Memorysize2/Memorygrow2 are ignored)
+        catch() calls Memorycopy3, args are ignored.
+ */
+
 
 
 #define I8(x)             (int)(M_[x])
@@ -77,5 +145,3 @@ static int Write(int f,int nf,int s,int n){if(!nf){swrite(1,M_+s,n);return 0;}
  char c=M_[f+nf];M_[f+nf]=0;int fd=open(M_+f,577,420);M_[f+nf]=c;if(fd<0)return -1;swrite(fd,M_+s,n);close(fd);return 0;}
 static int ReadIn(int d,int n){return(int)sread(1,M_+d,n);}
 static long Native(long x,long y){return(x+y)*0;}
-static void panic(int x){Exit(1);}
-
